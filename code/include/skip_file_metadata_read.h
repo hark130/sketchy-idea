3dc5232712b0e3d9a3b6e387bbeb430969a14651
hark130 <hark130@yahoo.com>
2024-03-05 15:40:26 -0600

    13-1: SKIP file metadata header initial commit
    Bad format but, at least, it currently lists out the general features I
    need to implement.
    
    TO DO: DON'T DO NOW...
    	- Determine appropriate return values
    	- Determine arguments
    	- Write the function comment blocks
    	- Order the functions alphabetically
    
    Changes to be committed:
    	new file:   code/include/skip_file_metadata_read.h

---
+++ code/include/skip_file_metadata_read.h
@@ -0,0 +1,134 @@
+#ifndef SKIP_FILE_METADATA_READ
+#define SKIP_FILE_METADATA_READ
+
+/*
+ *  Description:
+ *      
+ *  Args:
+ *      
+ *  Returns:
+ *      
+ */
+??? get_container_device_id();
+
+/*
+ *  Description:
+ *      
+ *  Args:
+ *      
+ *  Returns:
+ *      
+ */
+??? get_serial_num();
+
+/*
+ *  Description:
+ *      
+ *  Args:
+ *      
+ *  Returns:
+ *      
+ */
+??? get_file_perms();
+
+/*
+ *  Description:
+ *      
+ *  Args:
+ *      
+ *  Returns:
+ *      
+ */
+??? get_hard_link_num();
+
+/*
+ *  Description:
+ *      
+ *  Args:
+ *      
+ *  Returns:
+ *      
+ */
+??? get_owner();
+
+/*
+ *  Description:
+ *      
+ *  Args:
+ *      
+ *  Returns:
+ *      
+ */
+??? get_group();
+
+/*
+ *  Description:
+ *      
+ *  Args:
+ *      
+ *  Returns:
+ *      
+ */
+??? get_file_device_id();
+
+/*
+ *  Description:
+ *      
+ *  Args:
+ *      
+ *  Returns:
+ *      
+ */
+??? get_size();
+
+/*
+ *  Description:
+ *      
+ *  Args:
+ *      
+ *  Returns:
+ *      
+ */
+??? get_access_time();
+
+/*
+ *  Description:
+ *      
+ *  Args:
+ *      
+ *  Returns:
+ *      
+ */
+??? get_mod_time();
+
+/*
+ *  Description:
+ *      
+ *  Args:
+ *      
+ *  Returns:
+ *      
+ */
+??? get_change_time();
+
+/*
+ *  Description:
+ *      
+ *  Args:
+ *      
+ *  Returns:
+ *      
+ */
+??? get_block_size();
+
+/*
+ *  Description:
+ *      
+ *  Args:
+ *      
+ *  Returns:
+ *      
+ */
+??? get_block_count();
+
+#endif  /* SKIP_FILE_METADATA_READ */
