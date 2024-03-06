d784c4659e1c44a42a2f67c85def7fea601c3793
hark130 <hark130@yahoo.com>
2024-03-06 16:23:50 -0600

    13-1: Updated error macros
    Added __LINE__ refernces.
    Why haven't I done this sooner?!
    
    Changes to be committed:
    	modified:   code/include/skip_debug.h

--- code/include/skip_debug.h
+++ code/include/skip_debug.h
@@ -16,9 +16,9 @@
 
 #ifdef SKIP_DEBUG
 #include <string.h>  // strerror()
-#define PRINT_ERRNO(errorNum) if (errorNum) { fprintf(stderr, "<<<ERROR>>> - %s - %s() returned errno:\t%s\n", __FILE__, __FUNCTION_NAME__, strerror(errorNum)); };
-#define PRINT_ERROR(msg) do { fprintf(stderr, "<<<ERROR>>> - %s - %s() - %s!\n", __FILE__, __FUNCTION_NAME__, #msg); } while (0);
-#define PRINT_WARNG(msg) do { fprintf(stderr, "¿¿¿WARNING??? - %s - %s() - %s!\n", __FILE__, __FUNCTION_NAME__, #msg); } while (0);
+#define PRINT_ERRNO(errorNum) if (errorNum) { fprintf(stderr, "<<<ERROR>>> - %s - %s() - %d - Returned errno: %s\n", __FILE__, __FUNCTION_NAME__, __LINE__, strerror(errorNum)); };
+#define PRINT_ERROR(msg) do { fprintf(stderr, "<<<ERROR>>> - %s - %s() - %d - %s!\n", __FILE__, __FUNCTION_NAME__, __LINE__, #msg); } while (0);
+#define PRINT_WARNG(msg) do { fprintf(stderr, "¿¿¿WARNING??? - %s - %s() - %d - %s!\n", __FILE__, __FUNCTION_NAME__, __LINE__, #msg); } while (0);
 #else
 #define PRINT_ERRNO(errorNum) ;;;
 #define PRINT_ERROR(msg) ;;;
