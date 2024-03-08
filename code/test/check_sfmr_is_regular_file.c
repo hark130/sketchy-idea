3e9452c3caa0c211458070e3dbebe8e1eedee104
hark130 <hark130@yahoo.com>
2024-03-08 17:30:53 -0600

    13-1: Reordered the is_regular_file() test cases
    SPOILERS: I'll be copy/pasting these test cases among the other s_f_m_r
    library is_*() unit tests and I wanted them in some sort of discrenible
    order.
    
    TL;DR - I'm *almost* ready to start failing fast!
    
    Changes to be committed:
    	modified:   code/test/check_sfmr_is_regular_file.c
    
    Untracked files:
    	code/include/devops_code.h

--- code/test/check_sfmr_is_regular_file.c
+++ code/test/check_sfmr_is_regular_file.c
@@ -2,8 +2,11 @@
  *  Check unit test suit for skip_file_metadata_read.h's is_regular_file() function.
  */
 
-#include <stdlib.h>
 #include <check.h>                    // START_TEST(), END_TEST
+#include <limits.h>                   // PATH_MAX
+#include <stdio.h>                    // printf()
+#include <stdlib.h>
+#include <unistd.h>                   // get_current_dir_name()
 // Local includes
 #include "skip_file_metadata_read.h"  // is_character_device()
 
@@ -12,40 +15,7 @@
 #define CANARY_INT (int)0xBADC0DE  // Actually, a reverse canary value
 
 
-START_TEST(test_n01_regular_file)
-{
-    bool result = false;      // Return value from function call
-    int errnum = CANARY_INT;  // Errno from the function call
-    result = is_regular_file("./code/test/test_input/regular_file.txt", &errnum);
-    ck_assert(true == result);  // This input is a regular file
-    ck_assert_int_eq(0, errnum);  // The out param should be zeroized on success
-}
-END_TEST
-
-
-START_TEST(test_n02_directory)
-{
-    bool result = false;      // Return value from function call
-    int errnum = CANARY_INT;  // Errno from the function call
-    result = is_regular_file("./code/test/test_input/", &errnum);
-    ck_assert(false == result);  // This input is *NOT* a regular file
-    ck_assert_int_eq(0, errnum);  // The out param should be zeroized on success
-}
-END_TEST
-
-
-START_TEST(test_n03_symbolic_link)
-{
-    bool result = false;      // Return value from function call
-    int errnum = CANARY_INT;  // Errno from the function call
-    result = is_regular_file("./code/test/test_input/sym_link.txt", &errnum);
-    ck_assert(true == result);  // This may not be a regular file but stat() follows links
-    ck_assert_int_eq(0, errnum);  // The out param should be zeroized on success
-}
-END_TEST
-
-
-START_TEST(test_n04_block_device)
+START_TEST(test_n01_block_device)
 {
     bool result = false;      // Return value from function call
     int errnum = CANARY_INT;  // Errno from the function call
@@ -56,12 +26,46 @@ START_TEST(test_n04_block_device)
 END_TEST
 
 
-START_TEST(test_n05_character_device)
+START_TEST(test_n02_character_device)
 {
     bool result = false;      // Return value from function call
     int errnum = CANARY_INT;  // Errno from the function call
     result = is_regular_file("/dev/null", &errnum);
     ck_assert(false == result);  // This input is *NOT* a regular file
+    ck_assert_int_eq(0, errnum);  // The out param should be zeroized on success
+}
+END_TEST
+
+
+START_TEST(test_n03_directory)
+{
+    get_cwd();
+    bool result = false;      // Return value from function call
+    int errnum = CANARY_INT;  // Errno from the function call
+    result = is_regular_file("./code/test/test_input/", &errnum);
+    ck_assert(false == result);  // This input is *NOT* a regular file
+    ck_assert_int_eq(0, errnum);  // The out param should be zeroized on success
+}
+END_TEST
+
+
+START_TEST(test_n04_regular_file)
+{
+    bool result = false;      // Return value from function call
+    int errnum = CANARY_INT;  // Errno from the function call
+    result = is_regular_file("./code/test/test_input/regular_file.txt", &errnum);
+    ck_assert(true == result);  // This input is a regular file
+    ck_assert_int_eq(0, errnum);  // The out param should be zeroized on success
+}
+END_TEST
+
+
+START_TEST(test_n05_symbolic_link)
+{
+    bool result = false;      // Return value from function call
+    int errnum = CANARY_INT;  // Errno from the function call
+    result = is_regular_file("./code/test/test_input/sym_link.txt", &errnum);
+    ck_assert(true == result);  // This may not be a regular file but stat() follows links
     ck_assert_int_eq(0, errnum);  // The out param should be zeroized on success
 }
 END_TEST
@@ -77,11 +81,11 @@ Suite *is_regular_file_suite(void)
     /* Core test case */
     tc_core = tcase_create("Core");
 
-    tcase_add_test(tc_core, test_n01_regular_file);
-    tcase_add_test(tc_core, test_n02_directory);
-    tcase_add_test(tc_core, test_n03_symbolic_link);
-    tcase_add_test(tc_core, test_n04_block_device);
-    tcase_add_test(tc_core, test_n05_character_device);
+    tcase_add_test(tc_core, test_n01_block_device);
+    tcase_add_test(tc_core, test_n02_character_device);
+    tcase_add_test(tc_core, test_n03_directory);
+    tcase_add_test(tc_core, test_n04_regular_file);
+    tcase_add_test(tc_core, test_n05_symbolic_link);
     suite_add_tcase(suite, tc_core);
 
     return suite;
