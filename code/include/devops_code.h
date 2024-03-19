fab46e6478bf0bfd30436d357a102e87b7574513
hark130 <hark130@yahoo.com>
2024-03-19 14:28:32 -0500

    13-1: Updated devops_code header
    Renamed a function
    Updated a comment block
    etc
    
    Changes to be committed:
    	modified:   code/include/devops_code.h
    
    Changes not staged for commit:
    	modified:   code/test/check_sfmr_is_regular_file.c
    
    Untracked files:
    	code/src/devops_code.c

--- code/include/devops_code.h
+++ code/include/devops_code.h
@@ -6,6 +6,7 @@
 #define __SKIP_DEVOPS__
 
 #include <stdbool.h>    // bool, false, true
+#include <stddef.h>		// size_t
 
 // Baseline dir level to standardize file-based test input paths
 #define SKIP_REPO_NAME (const char *)"sketchy-idea"  // The name of this repo
@@ -41,21 +42,24 @@ int free_devops_mem(char **old_array);
 
 /*
  *  Description:
- *      Translate rel_filename into an absolute filename resolved to the root_dir, as extracted
+ *      Translate rel_filename into an absolute filename resolved to the repo_name, as extracted
  *		from the current working directory.  Caller is responsible for calling devops_free().
  *
  *  Args:
- *      root_dir: Root-level directory name to find in the current working directory.
- *      rel_filename: The relative filename to resolve to the root_dir.
- *		must_exist: If true, the resulting root_dir/rel_filename must exist or errnum will
+ *      repo_name: Root-level directory name to find in the current working directory
+ *			(e.g., SKIP_REPO_NAME).
+ *      rel_filename: Optional; The relative filename to resolve to repo_name's absolute path.
+ *			If this argument is NULL, only the repo_dir will be returned.
+ *		must_exist: If true, the resulting repo_dir/rel_filename must exist or errnum will
  *			be updated with ENOENT.  If false, it doesn't matter (e.g., "missing file" error input).
  *		errnum: [Out] Storage location for errno values encountered.
  *
  *  Returns:
- *      Heap-allocated array representing root_dir/rel_filename on success.  Caller is
+ *      Heap-allocated array representing repo_dir/rel_filename on success.  Caller is
  *		respsonsible for freeing the return value with free_devops_mem().
  *		NULL on error (check errnum).
  */
-char *get_repo_path(const char *root_dir, const char *rel_filename, bool must_exist, int *errnum);
+char *resolve_to_repo(const char *repo_name, const char *rel_filename, bool must_exist,
+                      int *errnum);
 
 #endif  /* __SKIP_DEVOPS__ */
