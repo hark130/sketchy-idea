7cd3c26afb3147cdb1ad7e4ca5da5f476e20ef96
hark130 <hark130@yahoo.com>
2024-03-19 15:36:07 -0500

    13-1: Done(?) w/ is_regular_file() unit test cases
    I think I might be done writing is_regular_file() unit test cases.
    All test cases passing.
    I think I'm failing fast.
    Time to fail even faster!  Time to write six more check_sfmr_is_*.c
    files!  THEN we'll be failing fast!
    
    Changes to be committed:
    	modified:   code/test/check_sfmr_is_regular_file.c

--- code/test/check_sfmr_is_regular_file.c
+++ code/test/check_sfmr_is_regular_file.c
@@ -23,6 +23,9 @@ code/dist/check_sfmr_is_regular_file.bin && CK_FORK=no valgrind --leak-check=ful
 #define CANARY_INT (int)0xBADC0DE  // Actually, a reverse canary value
 
 
+/**************************************************************************************************/
+/*************************************** NORMAL TEST CASES ****************************************/
+/**************************************************************************************************/
 START_TEST(test_n01_block_device)
 {
     bool result = false;      // Return value from function call
@@ -73,7 +76,40 @@ START_TEST(test_n03_directory)
 END_TEST
 
 
-START_TEST(test_n04_regular_file)
+START_TEST(test_n04_named_pipe)
+{
+    // LOCAL VARIABLES
+    const char *repo_name = SKIP_REPO_NAME;  // Repo name
+    bool result = false;                     // Return value from function call
+    int errnum = CANARY_INT;                 // Errno from the function calls
+    // Relative path for this test case's input
+    char input_rel_path[] = { "./code/test/test_input/named_pipe" };
+    // Absolute path for input_rel_path as resolved against the repo name
+    char *input_abs_path = resolve_to_repo(repo_name, input_rel_path, false, &errnum);
+
+    // VALIDATION
+    // It is important resolve_to_repo() succeeds
+    ck_assert_msg(0 == errnum, "resolve_to_repo(%s, %s) failed with [%d] %s", repo_name,
+                  input_rel_path, errnum, strerror(errnum));
+    remove_a_file(input_abs_path, true);  // Remove leftovers and ignore errors
+    errnum = make_a_pipe(input_abs_path);
+    ck_assert_msg(0 == errnum, "make_a_pipe(%s) failed with [%d] %s", input_abs_path,
+                  errnum, strerror(errnum));
+    errnum = CANARY_INT;  // Reset this temp var
+
+    // TEST START
+    result = is_regular_file(input_abs_path, &errnum);
+    ck_assert(false == result);  // This input is a regular file
+    ck_assert_int_eq(0, errnum);  // The out param should be zeroized on success
+
+    // CLEANUP
+    remove_a_file(input_abs_path, true);
+    free_devops_mem(&input_abs_path);
+}
+END_TEST
+
+
+START_TEST(test_n05_regular_file)
 {
     // LOCAL VARIABLES
     const char *repo_name = SKIP_REPO_NAME;  // Repo name
@@ -101,7 +137,18 @@ START_TEST(test_n04_regular_file)
 END_TEST
 
 
-START_TEST(test_n05_symbolic_link)
+START_TEST(test_n06_socket)
+{
+    bool result = false;      // Return value from function call
+    int errnum = CANARY_INT;  // Errno from the function call
+    result = is_regular_file("/var/run/dbus/system_bus_socket", &errnum);
+    ck_assert(false == result);  // This input is *NOT* a regular file
+    ck_assert_int_eq(0, errnum);  // The out param should be zeroized on success
+}
+END_TEST
+
+
+START_TEST(test_n07_symbolic_link)
 {
     // LOCAL VARIABLES
     const char *repo_name = SKIP_REPO_NAME;  // Repo name
@@ -128,6 +175,54 @@ START_TEST(test_n05_symbolic_link)
 }
 END_TEST
 
+
+/**************************************************************************************************/
+/**************************************** ERROR TEST CASES ****************************************/
+/**************************************************************************************************/
+START_TEST(test_e01_null_filename)
+{
+    bool result = false;      // Return value from function call
+    int errnum = CANARY_INT;  // Errno from the function call
+    result = is_regular_file(NULL, &errnum);
+    ck_assert(false == result);  // This input is *NOT* a regular file
+    ck_assert_int_eq(EINVAL, errnum);  // The out param should be zeroized on success
+}
+END_TEST
+
+
+START_TEST(test_e02_empty_filename)
+{
+    bool result = false;      // Return value from function call
+    int errnum = CANARY_INT;  // Errno from the function call
+    result = is_regular_file("\0 NOT HERE!", &errnum);
+    ck_assert(false == result);  // This input is *NOT* a regular file
+    ck_assert_int_eq(EINVAL, errnum);  // The out param should be zeroized on success
+}
+END_TEST
+
+
+START_TEST(test_e03_null_errnum)
+{
+    bool result = false;  // Return value from function call
+    result = is_regular_file("/dev/loop0", NULL);
+    ck_assert(false == result);  // This input is *NOT* a regular file
+}
+END_TEST
+
+
+/**************************************************************************************************/
+/*************************************** SPECIAL TEST CASES ***************************************/
+/**************************************************************************************************/
+START_TEST(test_s01_missing_filename)
+{
+    bool result = false;      // Return value from function call
+    int errnum = CANARY_INT;  // Errno from the function call
+    result = is_regular_file("/does/not/exist.txt", &errnum);
+    ck_assert(false == result);  // This input is *NOT* a regular file
+    ck_assert_int_eq(ENOENT, errnum);  // The out param should be zeroized on success
+}
+END_TEST
+
  
 Suite *is_regular_file_suite(void)
 {
@@ -142,8 +237,14 @@ Suite *is_regular_file_suite(void)
     tcase_add_test(tc_core, test_n01_block_device);
     tcase_add_test(tc_core, test_n02_character_device);
     tcase_add_test(tc_core, test_n03_directory);
-    tcase_add_test(tc_core, test_n04_regular_file);
-    tcase_add_test(tc_core, test_n05_symbolic_link);
+    tcase_add_test(tc_core, test_n04_named_pipe);
+    tcase_add_test(tc_core, test_n05_regular_file);
+    tcase_add_test(tc_core, test_n06_socket);
+    tcase_add_test(tc_core, test_n07_symbolic_link);
+    tcase_add_test(tc_core, test_e01_null_filename);
+    tcase_add_test(tc_core, test_e02_empty_filename);
+    tcase_add_test(tc_core, test_e03_null_errnum);
+    tcase_add_test(tc_core, test_s01_missing_filename);
     suite_add_tcase(suite, tc_core);
 
     return suite;
