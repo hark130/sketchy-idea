48f52e4cfb89b55efad77eb3017cce7de8ba616f
hark130 <hark130@yahoo.com>
2024-04-08 18:00:43 -0500

    13-2: Initial commit of set_atime_now() unit tests
    I'm workiing through some testisms.
    For instance, directory atimes aren't necessarily updated on a
    filesystem mounted with the relatime flag (like mine).
    
    I'm currently working through some permission issues with the named
    pipe.  My code is creating the named pipe with permissions 1200 and I
    think that's stopping the atime from being updated (based on when atimes
    can be updated).
    
    Where do I pick up in the future?  The test code compiles but currrently
    is failing three unit tests.  Pick it back up there.
    
    That being said, this may be some of my best Check unit test code.  The
    test cases are a lot shorter because the test code is actually pretty
    modulear.  TO DO: DON'T DO NOW... Write a Check library.
    
    Changes to be committed:
    	new file:   code/test/check_sfmw_set_atime_now.c

---
+++ code/test/check_sfmw_set_atime_now.c
@@ -0,0 +1,429 @@
+/*
+ *  Check unit test suit for skid_file_metadata_write.h's set_atime_now() function.
+ *
+ *  Copy/paste the following from the repo's top-level directory...
+
+make -C code dist/check_sfmw_set_atime_now.bin
+code/dist/check_sfmw_set_atime_now.bin && CK_FORK=no valgrind --leak-check=full --show-leak-kinds=all code/dist/check_sfmw_set_atime_now.bin
+
+ *
+ */
+
+#include <check.h>                     // START_TEST(), END_TEST
+#include <limits.h>                    // PATH_MAX
+#include <stdio.h>                     // printf()
+#include <stdlib.h>
+#include <unistd.h>                    // get_current_dir_name(), usleep()
+// Local includes
+#include "devops_code.h"               // resolve_to_repo(), SKID_REPO_NAME
+#include "skid_file_metadata_read.h"   // get_access_time()
+#include "skid_file_metadata_write.h"  // set_atime_now()
+
+
+// Use this to help highlight an errnum that wasn't updated
+#define CANARY_INT (int)0xBADC0DE  // Actually, a reverse canary value
+// Use this with usleep(MICRO_SEC_SLEEP) to let the file timestamp update
+#define MICRO_SEC_SLEEP (useconds_t)5  // Give the file time a 5 microseconds to update
+
+
+/**************************************************************************************************/
+/***************************************** TEST FIXTURES ******************************************/
+/**************************************************************************************************/
+
+
+char *test_pipe_path;  // Heap array containing the absolute pipe filename resolved to the repo
+char *test_socket_path;  // Heap array containing the absolute socket filename resolved to the repo
+
+/*
+ *	Resolve paththame to SKID_REPO_NAME in a standardized way.  Use free_devops_mem() to free
+ *	the return value.
+ */
+char *resolve_test_input(const char *pathname);
+
+/*
+ *	1. Gets the original time, 2. Calls the tested function, 3. Gets the new time.
+ *	Verifies: actual return == exp_ret, original time < new time.
+ */
+void run_test_case(const char *pathname, bool follow_sym, int exp_ret);
+
+/*
+ *	Resolve the named pipe and raw socket default filenames to the repo and store the heap
+ *	memory pointer in the globals.
+ */
+void setup(void);
+
+/*
+ *	Delete the named pipe and raw socket files.  Then, free the heap memory arrays.
+ */
+void teardown(void);
+
+
+char *resolve_test_input(const char *pathname)
+{
+	// LOCAL VARIABLES
+	int errnum = CANARY_INT;                 // Errno values
+	const char *repo_name = SKID_REPO_NAME;  // Name of the repo
+	char *resolved_name = NULL;              // pathname resolved to repo_name
+
+	// RESOLVE IT
+	resolved_name = resolve_to_repo(repo_name, pathname, false, &errnum);
+	ck_assert_msg(0 == errnum, "resolve_to_repo(%s, %s) failed with [%d] %s", repo_name,
+				  pathname, errnum, strerror(errnum));
+	ck_assert_msg(NULL != resolved_name, "resolve_to_repo(%s, %s) failed to resolve the path",
+				  repo_name, pathname);
+
+	// DONE
+	if (0 != errnum && resolved_name)
+	{
+		free_devops_mem(&resolved_name);  // Best effort
+	}
+	return resolved_name;
+}
+
+
+void run_test_case(const char *pathname, bool follow_sym, int exp_ret)
+{
+	// LOCAL VARIABLES
+	int errnum = CANARY_INT;  // Errno values
+	time_t old_time = 0;      // Original time
+	time_t new_time = 0;      // New time
+
+	// RUN IT
+	// No need to compare times if the expected result is "error"
+	if (0 == exp_ret)
+	{
+		// 1. Get original time
+		old_time = get_shell_atime(pathname, &errnum);
+		ck_assert_msg(0 == errnum, "old get_shell_atime(%s) failed with [%d] %s",
+					  pathname, errnum, strerror(errnum));
+		errnum = CANARY_INT;  // Reset temp variable
+		micro_sleep(MICRO_SEC_SLEEP);  // Wait a few microseconds
+	}
+	// 2. Call function
+	errnum = set_atime_now(pathname, follow_sym);
+	ck_assert_msg(exp_ret == errnum, "set_atime_now(%s) returned [%d] '%s' instead of [%d] '%s",
+		          pathname, errnum, strerror(errnum), exp_ret, strerror(exp_ret));
+	errnum = CANARY_INT;  // Reset temp variable
+	// No need to compare times if the expected result is "error"
+	if (0 == exp_ret)
+	{
+		// 3. Get current time
+		micro_sleep(MICRO_SEC_SLEEP);  // Wait a few microseconds
+		new_time = get_shell_atime(pathname, &errnum);
+		ck_assert_msg(0 == errnum, "new get_shell_atime(%s) failed with [%d] %s",
+					  pathname, errnum, strerror(errnum));
+
+		// VALIDATE RESULTS
+		// Compare times
+		/*
+		 *	WARNING: Filesystems are frequently mounted with the relatime flag.  Mine certainly is.
+		 *	See: cat /proc/mounts
+		 *
+		 *	Why does that matter?  Great question.  When your filesystem is mounted with
+		 *	relatime, and you read a file or directory, the atime is only updated if:
+		 *		- the atime is older than the ctime (change time) or mtime (modification time), or
+		 *		- if the atime is more than a day in the past.
+		 *
+		 *	What does that have to do with these unit tests?  Another great question.
+		 *	I could have launched into some crazy filesytem-parsing devops code to better
+		 *	when a directory's timestamp should be updated (and when it shouldn't).  Instead,
+		 *	I decided to go 'easy' on validating directories based on what I've found.
+		 */
+		if (true == is_directory(pathname, &errnum))
+		{
+			ck_assert_msg(old_time <= new_time, "The time for dir %s was not updated", pathname);
+		}
+		else
+		{
+			ck_assert_msg(old_time < new_time, "The time for %s was not updated", pathname);
+		}
+		ck_assert_msg(0 == errnum, "new is_directory(%s) failed with [%d] %s",
+					  pathname, errnum, strerror(errnum));
+	}
+}
+
+
+void setup(void)
+{
+	// LOCAL VARIABLES
+	int errnum = CANARY_INT;                                      // Errno values
+	char named_pipe[] = { "./code/test/test_input/named_pipe" };  // Default test input: pipe
+	char raw_socket[] = { "./code/test/test_input/raw_socket" };  // Default test input: socket
+
+	// SET IT UP
+	// Named Pipe
+	test_pipe_path = resolve_test_input(named_pipe);
+	if (test_pipe_path)
+	{
+		remove_a_file(test_pipe_path, true);  // Remove leftovers and ignore errors
+		errnum = make_a_pipe(test_pipe_path);
+		ck_assert_msg(0 == errnum, "make_a_pipe(%s) failed with [%d] %s", test_pipe_path,
+					  errnum, strerror(errnum));
+		errnum = CANARY_INT;  // Reset temp variable
+	}
+	errnum = CANARY_INT;  // Reset temp variable
+	// Raw Socket
+	test_socket_path = resolve_test_input(raw_socket);
+	if (test_socket_path)
+	{
+		remove_a_file(test_socket_path, true);  // Remove leftovers and ignore errors
+		errnum = make_a_socket(test_socket_path);
+		ck_assert_msg(0 == errnum, "make_a_socket(%s) failed with [%d] %s", test_socket_path,
+					  errnum, strerror(errnum));
+		errnum = CANARY_INT;  // Reset temp variable
+	}
+
+	// DONE
+	return;
+}
+
+
+void teardown(void)
+{
+	// Pipe
+	// remove_a_file(test_pipe_path, true);  // Best effort
+	free_devops_mem(&test_pipe_path);  // Ignore any errors
+	// Socket
+	// remove_a_file(test_socket_path, true);  // Best effort
+	free_devops_mem(&test_socket_path);  // Ignore any errors
+}
+
+
+/**************************************************************************************************/
+/*************************************** NORMAL TEST CASES ****************************************/
+/**************************************************************************************************/
+
+
+START_TEST(test_n01_directory)
+{
+	// LOCAL VARIABLES
+	bool follow = true;  // Follow symlinks
+	int exp_result = 0;  // Expected results
+	// Absolute path for test input as resolved against the repo name
+	char *input_abs_path = resolve_test_input("./code/test/test_input/");
+
+	// RUN TEST
+	run_test_case(input_abs_path, follow, exp_result);
+
+	// CLEANUP
+	free_devops_mem(&input_abs_path);
+}
+END_TEST
+
+
+START_TEST(test_n02_named_pipe)
+{
+	// LOCAL VARIABLES
+	bool follow = true;  // Follow symlinks
+	int exp_result = 0;  // Expected results
+	// Absolute path for test input as resolved against the repo name
+	char *input_abs_path = test_pipe_path;
+
+	// RUN TEST
+	run_test_case(input_abs_path, follow, exp_result);
+}
+END_TEST
+
+
+START_TEST(test_n03_regular_file)
+{
+	// LOCAL VARIABLES
+	bool follow = true;  // Follow symlinks
+	int exp_result = 0;  // Expected results
+	// Absolute path for test input as resolved against the repo name
+	char *input_abs_path = resolve_test_input("./code/test/test_input/regular_file.txt");
+
+	// RUN TEST
+	run_test_case(input_abs_path, follow, exp_result);
+
+	// CLEANUP
+	free_devops_mem(&input_abs_path);
+}
+END_TEST
+
+
+START_TEST(test_n04_socket)
+{
+	// LOCAL VARIABLES
+	bool follow = true;  // Follow symlinks
+	int exp_result = 0;  // Expected results
+	// Absolute path for test input as resolved against the repo name
+	char *input_abs_path = test_socket_path;
+
+	// RUN TEST
+	run_test_case(input_abs_path, follow, exp_result);
+}
+END_TEST
+
+
+START_TEST(test_n05_symbolic_link_follow)
+{
+	// LOCAL VARIABLES
+	bool follow = true;       // Follow symlinks
+	int result = 0;        // Return value from function call
+	int errnum = CANARY_INT;  // Errno from the function calls
+	int exp_result = 0;    // Expected results
+	// Absolute path for test input as resolved against the repo name
+	char *input_abs_path = resolve_test_input("./code/test/test_input/sym_link.txt");
+	// Absolute path for actual_rel_path as resolved against the repo name
+	char *actual_abs_path = resolve_test_input("./code/test/test_input/regular_file.txt");
+
+	// TEST START
+	result = set_atime_now(input_abs_path, follow);
+	ck_assert_msg(exp_result == result, "set_atime_now(%s) returned %d instead of %d",
+				  input_abs_path, result, exp_result);
+	ck_assert_int_eq(0, errnum);  // The out param should be zeroized on success
+
+	// CLEANUP
+	free_devops_mem(&input_abs_path);
+	free_devops_mem(&actual_abs_path);
+}
+END_TEST
+
+
+/**************************************************************************************************/
+/**************************************** ERROR TEST CASES ****************************************/
+/**************************************************************************************************/
+START_TEST(test_e01_null_filename)
+{
+	// LOCAL VARIABLES
+	bool follow = true;           // Follow symlinks
+	int exp_result = EINVAL;      // Expected results
+	char *input_abs_path = NULL;  // Test case input
+
+	// RUN TEST
+	run_test_case(input_abs_path, follow, exp_result);
+}
+END_TEST
+
+
+START_TEST(test_e02_empty_filename)
+{
+	// LOCAL VARIABLES
+	bool follow = true;                          // Follow symlinks
+	int exp_result = EINVAL;                     // Expected results
+	char input_abs_path[] = { "\0 NOT HERE!" };  // Test case input
+
+	// RUN TEST
+	run_test_case(input_abs_path, follow, exp_result);
+}
+END_TEST
+
+
+/**************************************************************************************************/
+/*************************************** SPECIAL TEST CASES ***************************************/
+/**************************************************************************************************/
+START_TEST(test_s01_missing_filename)
+{
+	// LOCAL VARIABLES
+	bool follow = true;                                 // Follow symlinks
+	int exp_result = ENOENT;                            // Expected results
+	char input_abs_path[] = { "/does/not/exist.txt" };  // Test case input
+
+	// RUN TEST
+	run_test_case(input_abs_path, follow, exp_result);
+}
+END_TEST
+
+
+START_TEST(test_s02_symbolic_link_nofollow)
+{
+	// LOCAL VARIABLES
+	bool follow = false;  // Follow symlinks
+	int exp_result = 0;   // Expected results
+	// Absolute path for test input as resolved against the repo name
+	char *input_abs_path = resolve_test_input("./code/test/test_input/sym_link.txt");
+	// Absolute path for actual_rel_path as resolved against the repo name
+	char *actual_abs_path = resolve_test_input("./code/test/test_input/regular_file.txt");
+
+	// RUN TEST
+	run_test_case(input_abs_path, follow, exp_result);
+
+	// CLEANUP
+	free_devops_mem(&input_abs_path);
+	free_devops_mem(&actual_abs_path);
+}
+END_TEST
+ 
+
+START_TEST(test_s03_block_device)
+{
+	// LOCAL VARIABLES
+	bool follow = true;                        // Follow symlinks
+	int exp_result = EPERM;                    // Expected results
+	char input_abs_path[] = { "/dev/loop0" };  // Test case input
+
+	// RUN TEST
+	run_test_case(input_abs_path, follow, exp_result);
+}
+END_TEST
+
+
+START_TEST(test_s04_character_device)
+{
+	// LOCAL VARIABLES
+	bool follow = true;                       // Follow symlinks
+	int exp_result = EPERM;                   // Expected results
+	char input_abs_path[] = { "/dev/null" };  // Test case input
+
+	// RUN TEST
+	run_test_case(input_abs_path, follow, exp_result);
+}
+END_TEST
+
+
+Suite *set_atime_now_suite(void)
+{
+	Suite *suite = NULL;
+	TCase *tc_core = NULL;
+
+	suite = suite_create("SFMW_Set_Atime_Now");
+
+	/* Core test case */
+	tc_core = tcase_create("Core");
+	tcase_add_checked_fixture(tc_core, setup, teardown);
+	tcase_add_test(tc_core, test_n01_directory);
+	tcase_add_test(tc_core, test_n02_named_pipe);
+	tcase_add_test(tc_core, test_n03_regular_file);
+	tcase_add_test(tc_core, test_n04_socket);
+	tcase_add_test(tc_core, test_n05_symbolic_link_follow);
+	tcase_add_test(tc_core, test_e01_null_filename);
+	tcase_add_test(tc_core, test_e02_empty_filename);
+	tcase_add_test(tc_core, test_s01_missing_filename);
+	tcase_add_test(tc_core, test_s02_symbolic_link_nofollow);
+	tcase_add_test(tc_core, test_s03_block_device);
+	tcase_add_test(tc_core, test_s04_character_device);
+	suite_add_tcase(suite, tc_core);
+
+	return suite;
+}
+
+
+int main(void)
+{
+	// LOCAL VARIABLES
+	int errnum = 0;  // Errno from the function call
+	// Relative path for this test case's input
+	char log_rel_path[] = { "./code/test/test_output/check_sfmw_set_atime_now.log" };
+	// Absolute path for log_rel_path as resolved against the repo name
+	char *log_abs_path = resolve_to_repo(SKID_REPO_NAME, log_rel_path, false, &errnum);
+	int number_failed = 0;
+	Suite *suite = NULL;
+	SRunner *suite_runner = NULL;
+
+	// SETUP
+	suite = set_atime_now_suite();
+	suite_runner = srunner_create(suite);
+	srunner_set_log(suite_runner, log_abs_path);
+
+	// RUN IT
+	srunner_run_all(suite_runner, CK_NORMAL);
+	number_failed = srunner_ntests_failed(suite_runner);
+
+	// CLEANUP
+	srunner_free(suite_runner);
+	free_devops_mem(&log_abs_path);
+
+	// DONE
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
