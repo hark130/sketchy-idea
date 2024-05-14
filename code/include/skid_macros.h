4e184d475232e87540d4f0962a49e47128188ace
hark130 <hark130@yahoo.com>
2024-05-14 16:40:09 -0500

    13-6: Extricated common SKID macros
    Created a MACRO	header for SKID
    Extricated mode_t macros from s_f_m_w.h into the new macros header.
    
    I'll eventually find out what I broke...
    
    Changes to be committed:
    	modified:   code/include/skid_file_metadata_write.h
    	new file:   code/include/skid_macros.h
    
    Untracked files:
    	code/test/check_sdo_create_dir.c

---
+++ code/include/skid_macros.h
@@ -0,0 +1,27 @@
+/*
+ *	Defines common use macros for use with SKETCHY IDEA (SKID).
+ */
+
+#ifndef __SKID_MACROS__
+#define __SKID_MACROS__
+
+// You may use these macros with SKID mode_t arguments.
+/* We translated the chown(2) macros so you don't have to! */
+// Owner Permissions
+#define SKID_MODE_OWNER_R S_IRUSR  // Read by owner
+#define SKID_MODE_OWNER_W S_IWUSR  // Write by owner
+#define SKID_MODE_OWNER_X S_IXUSR  // Execute/search by owner
+// Group Permissions
+#define SKID_MODE_GROUP_R S_IRGRP  // Read by group
+#define SKID_MODE_GROUP_W S_IWGRP  // Write by group
+#define SKID_MODE_GROUP_X S_IXGRP  // Execute/search by group
+// Other Permissions
+#define SKID_MODE_OTHER_R S_IROTH  // Read by others
+#define SKID_MODE_OTHER_W S_IWOTH  // Write by others
+#define SKID_MODE_OTHER_X S_IXOTH  // Execute/search by others
+// Special Permissions
+#define SKID_MODE_SET_UID S_ISUID  // set-user-ID
+#define SKID_MODE_SET_GID S_ISGID  // set-user-ID
+#define SKID_MODE_STICKYB S_ISVTX  // sticky bit (see: unlink(2), restricted deletion flag)
+
+#endif  /* __SKID_MACROS__ */
