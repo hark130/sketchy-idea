2f53f289e7faf6ca22e0255922a422f52dd85f6a
hark130 <hark130@yahoo.com>
2024-07-01 15:22:19 -0500

    SKID-8: Initial commit of new validation library
    Time to stop copy/pasting and start getting real.
    
    Uncompiled.
    
    Changes to be committed:
    	new file:   code/include/skid_validation.h
    	new file:   code/src/skid_validation.c

---
+++ code/include/skid_validation.h
@@ -0,0 +1,53 @@
+#ifndef __SKID_VALIDATION__
+#define __SKID_VALIDATION__
+
+/*
+ *  Description:
+ *      Validates the errnum arguments on behalf of SKID.
+ *
+ *  Args:
+ *      err: A non-NULL pointer to an integer.
+ *
+ *  Returns:
+ *      An errno value indicating the results of validation.  0 on successful validation.
+ */
+int validate_skid_err(int *err);
+
+/*
+ *  Description:
+ *      Validate file descriptors on behalf of SKID.
+ *
+ *  Args:
+ *      fd: File descriptor to validate.
+ *
+ *  Returns:
+ *      0 on success, errno on failed validation. 
+ */
+int validate_skid_fd(int fd);
+
+/*
+ *  Description:
+ *      Validate socket file descriptors on behalf of SKID.
+ *
+ *  Args:
+ *      sockfd: Socket file descriptor to validate.
+ *
+ *  Returns:
+ *      0 on success, errno on failed validation. 
+ */
+int validate_skid_sockfd(int sockfd);
+
+/*
+ *  Description:
+ *      Validate C strings on behalf of SKID.
+ *
+ *  Args:
+ *      string: The C string to validate.
+ *      can_be_empty: If true, string may be empty.  Otherwise, the string must have a length.
+ *
+ *  Returns:
+ *      0 on success, errno on failed validation.
+ */
+int validate_skid_string(const char *string, bool can_be_empty);
+
+#endif  /* __SKID_VALIDATION__ */
---
+++ code/src/skid_validation.c
@@ -0,0 +1,95 @@
+/*
+ *	This library defines functionality to help automate validation.
+ */
+
+#define SKID_DEBUG						// Enable DEBUG logging
+
+#include "skid_debug.h"				  	// PRINT_ERRNO(), PRINT_ERROR()
+#include "skid_macros.h"				// ENOERR
+#include "skid_validation.h"			// 
+#include <errno.h>						// EINVAL
+
+
+/**************************************************************************************************/
+/********************************* PRIVATE FUNCTION DECLARATIONS **********************************/
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/********************************** PUBLIC FUNCTION DEFINITIONS ***********************************/
+/**************************************************************************************************/
+
+
+int validate_skid_err(int *err)
+{
+	// LOCAL VARIABLES
+	int result = ENOERR;  // The results of validation
+
+	// INPUT VALIDATION
+	if (!err)
+	{
+		result = EINVAL;  // NULL pointer
+	}
+
+	// DONE
+	return result;
+}
+
+
+int validate_skid_fd(int fd)
+{
+	// LOCAL VARIABLES
+	int result = EBADF;  // Validation result
+
+	// INPUT VALIDATION
+	if (fd >= 0)
+	{
+		result = ENOERR;  // Good(?).
+	}
+
+	// DONE
+	return result;
+}
+
+
+int validate_skid_sockfd(int sockfd)
+{
+	// LOCAL VARIABLES
+	int result = EBADF;  // Validation result
+
+	// INPUT VALIDATION
+	if (sockfd >= 0)
+	{
+		result = ENOERR;  // Good(?).
+	}
+
+	// DONE
+	return result;
+}
+
+
+int validate_skid_string(const char *string, bool can_be_empty)
+{
+	// LOCAL VARIABLES
+	int result = ENOERR;  // Validation result
+
+	// INPUT VALIDATION
+	if (NULL == string)
+	{
+		result = EINVAL;  // NULL pointer
+	}
+	else if (false == can_be_empty)
+	{
+		if (0x0 == *string)
+		{
+			result = EINVAL;  // Empty string
+		}
+	}
+
+	// DONE
+	return result;
+}
+
+
+/**************************************************************************************************/
+/********************************** PRIVATE FUNCTION DEFINITIONS **********************************/
+/**************************************************************************************************/
