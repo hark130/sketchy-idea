3bae9e175f97d1b154f7a06c5c37b9d2cfe395b8
hark130 <hark130@yahoo.com>
2024-08-01 15:43:49 -0500

    SKID-8: Big simple dgram client refactor
    Just about scrapped the old code.
    New code compiles and appears to work (but the server isn't receiving
    anything).
    I'm committing so I can try this refactor on entirely different machine.
    
    Changes to be committed:
    	modified:   code/test/test_sn_simple_dgram_client.c
    
    Changes not staged for commit:
    	modified:   code/test/test_sn_simple_dgram_server.c

--- code/test/test_sn_simple_dgram_client.c
+++ code/test/test_sn_simple_dgram_client.c
@@ -13,7 +13,9 @@
 
 #define SKID_DEBUG					// Enable DEBUG logging
 
+#include <arpa/inet.h>				// inet_addr()
 #include <errno.h>					// EINVAL
+#include <netinet/in.h>				// inet_addr()
 #include <stdio.h>					// fprintf()
 #include <stdlib.h>					// exit()
 #include <sys/socket.h>				// AF_INET
@@ -26,23 +28,25 @@
 #define SERVER_DOMAIN AF_INET 	 	// Server socket domain
 #define SERVER_TYPE SOCK_DGRAM		// Server socket type
 #define SERVER_PROTOCOL IPPROTO_UDP	// Server socket protocol
-#define SERVER_PORT "5678"			// The port clients will connect to
+#define SERVER_PORT 5678			// The port clients will connect to
 
 
 int main(int argc, char *argv[])
 {
 	// LOCAL VARIABLES
 	int exit_code = 0;                           // Store errno and/or results here
-	int result = 0;                              // Additional errno values
+	// int result = 0;                              // Additional errno values
 	int server_domain = SERVER_DOMAIN;           // Server socket domain
 	int server_type = SERVER_TYPE;               // Server socket type
-	int server_fd = SKID_BAD_FD;                 // Server file descriptor
-	const char *node = NULL;                     // Hostname/IP of the server (NULL for localhost)
-	struct addrinfo hints;                       // Selection criteria
-	struct addrinfo *servinfo = NULL;            // Out argument for get_addr_info()
-	struct addrinfo *temp_serv = NULL;           // Use this to walk the servinfo linked list
+	unsigned short server_port = SERVER_PORT;    // Port the server is listening on
+	int socket_fd = SKID_BAD_FD;                 // Server file descriptor
+	const char *node = "127.0.0.1";              // Hostname/IP of the server
+	// struct addrinfo hints;                       // Selection criteria
+	// struct addrinfo *servinfo = NULL;            // Out argument for get_addr_info()
+	// struct addrinfo *temp_serv = NULL;           // Use this to walk the servinfo linked list
 	char message[] = { "Hello, world!" };        // Message for the client to send to the server
 	int sendto_flags = 0;                        // See sendto(2)
+	struct sockaddr_in servaddr;
 
 	// INPUT VALIDATION
 	if (argc == 2)
@@ -55,81 +59,48 @@ int main(int argc, char *argv[])
 	   exit_code = EINVAL;
 	}
 
-	// SETUP SERVER
-	// Setup hints
+	// SETUP
 	if (!exit_code)
 	{
 		// Zeroize the struct
-		memset(&hints, 0x0, sizeof(hints));
-	    hints.ai_family = server_domain;  // AKA domain
-	    hints.ai_socktype = server_type;
-	    hints.ai_flags = AI_PASSIVE; // use my IP
+		memset(&servaddr, 0x0, sizeof(servaddr));
+		servaddr.sin_family = server_domain;
+		servaddr.sin_addr.s_addr = inet_addr(node);
+		servaddr.sin_port = htons(server_port);
 	}
-	// Get an address
+
+	// CONNECT
+	// Open Socket
 	if (!exit_code)
 	{
-		exit_code = get_addr_info(node, SERVER_PORT, &hints, &servinfo);
-	}
-	// Connect
-	if (!exit_code)
-	{
-		for (temp_serv = servinfo; NULL != temp_serv; temp_serv = temp_serv->ai_next)
+		// Open the socket
+		socket_fd = open_socket(server_domain, server_type, 0, &exit_code);
+		if (SKID_BAD_FD == socket_fd)
 		{
-			// Open the socket
-			server_fd = open_socket(temp_serv->ai_family, temp_serv->ai_socktype,
-				                    temp_serv->ai_protocol, &exit_code);
-			if (SKID_BAD_FD == server_fd)
-			{
-				// Failed to open the socket with this domain, type, and protocol
-				PRINT_ERROR(The call to open_socket() failed);
-				PRINT_ERRNO(exit_code);
-				FPRINTF_ERR("%s - The call was open_socket(%d, %d, %d, %p)\n", DEBUG_ERROR_STR,
-					        temp_serv->ai_family, temp_serv->ai_socktype, temp_serv->ai_protocol,
-					        &exit_code);
-				continue;  // Try the next node in the linked list
-			}
-			else
-			{
-				// Bind
-				exit_code = bind_struct(server_fd, temp_serv->ai_addr, temp_serv->ai_addrlen);
-				if (exit_code)
-				{
-					// Failed to connect the socket to the address
-					PRINT_ERROR(The call to bind_struct() failed);
-					PRINT_ERRNO(exit_code);
-					FPRINTF_ERR("%s - The call was bind_struct(%d, %p, %d)\n", DEBUG_ERROR_STR,
-						        server_fd, temp_serv->ai_addr, temp_serv->ai_addrlen);
-					close_socket(&server_fd, false);  // This socket is no good
-					continue;  // Try the next node in the linked list
-				}
-				else
-				{
-					break;  // We got one!
-				}
-			}
-		}
-		if (NULL == temp_serv)
-		{
-			FPRINTF_ERR("%s - Client: failed to bind to a server\n", DEBUG_ERROR_STR);
-			exit_code = ETIMEDOUT;  // It's as close as anything else
-		}
-	}
-	// Success or failure, we're done with the addrinfo linked list
-	if (NULL != servinfo)
-	{
-		result = free_addr_info(&servinfo);
-		if (!exit_code)
-		{
-			exit_code = result;
+			// Failed to open the socket with this domain, type, and protocol
+			PRINT_ERROR(The call to open_socket() failed);
+			PRINT_ERRNO(exit_code);
+			FPRINTF_ERR("%s - The call was open_socket(%d, %d, %d, %p)\n", DEBUG_ERROR_STR,
+				        server_domain, server_type, 0, &exit_code);
 		}
 	}
 
-	// CLIENT
+	// Connect
+	if (!exit_code)
+	{
+		exit_code = connect_socket(socket_fd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+		if (exit_code)
+		{
+			PRINT_ERROR(The call to connect_socket() failed);
+			PRINT_ERRNO(exit_code);
+		}
+	}
+
+	// Send
 	if (!exit_code)
 	{
 		FPRINTF_ERR("%s - Client: attempting to send data...\n", DEBUG_INFO_STR);
-        exit_code = send_to_socket(server_fd, message, sendto_flags, temp_serv->ai_addr,
-								   temp_serv->ai_addrlen);
+        exit_code = send_to_socket(socket_fd, message, sendto_flags, (struct sockaddr *)NULL, 0);
         if (!exit_code)
         {
 			FPRINTF_ERR("%s - Client: message sent!\n", DEBUG_INFO_STR);
@@ -142,7 +113,7 @@ int main(int argc, char *argv[])
 	}
 
 	// CLEANUP
-	close_socket(&server_fd, true);  // Best effort
+	close_socket(&socket_fd, true);  // Best effort
 
 	// DONE
 	exit(exit_code);
