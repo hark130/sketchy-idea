9b6e86f55cbfb4b7da2dddada74b6bf8ed077021
hark130 <hark130@yahoo.com>
2024-12-06 15:37:51 -0600

    1-3: Added declared functions to signals header
    Added function declarations to block and unblock signals.
    
    Changes to be committed:
    	modified:   code/include/skid_signals.h
    
    Changes not staged for commit:
    	modified:   code/src/skid_signals.c

--- code/include/skid_signals.h
+++ code/include/skid_signals.h
@@ -6,6 +6,20 @@
 
 /*
  *	Description:
+ *		Block a signal.  The result will be the union of the current set and signum.
+ *		It is not possible to block SIGKILL or SIGSTOP.  Attempts to do so are silently ignored.
+ *
+ *	Args:
+ *		signum: The signal number.
+ *		oldset: [Optional] If non-NULL, the previous set will be saved here.
+ *
+ *	Returns:
+ *		On success, ENOERR is returned.  On error, errno is returned.
+ */
+int block_signal(int signum, sigset_t *oldset);
+
+/*
+ *	Description:
  *		Establish handler as the signum signal handler.
  *
  *	Args:
@@ -34,6 +48,21 @@ int set_signal_handler(int signum, SignalHandler handler, int flags, struct siga
  *	Returns:
  *		On success, ENOERR is returned.  On error, errno is returned.
  */
-int set_signal_handler_ext(int signum, SignalHandlerExt handler, int flags, struct sigaction *oldact);
+int set_signal_handler_ext(int signum, SignalHandlerExt handler, int flags,
+	                       struct sigaction *oldact);
+
+/*
+ *	Description:
+ *		Unblock a signal.  The signal number will be removed from the current set of blocked
+ *		signals.  It is permissible to attempt to unblock a signal which is not blocked.
+ *
+ *	Args:
+ *		signum: The signal number.
+ *		oldset: [Optional] If non-NULL, the previous set will be saved here.
+ *
+ *	Returns:
+ *		On success, ENOERR is returned.  On error, errno is returned.
+ */
+int unblock_signal(int signum, sigset_t *oldset);
 
 #endif  /* __SKID_SIGNALS__ */
