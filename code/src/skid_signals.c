ed742c72efde7ecd3541359a1d726c761956bcee
hark130 <hark130@yahoo.com>
2024-12-06 15:38:52 -0600

    1-3: Defined new signals module functionality
    Defined both public and "private" functions to implement the new block
    and unblock features.
    
    Changes to be committed:
    	modified:   code/src/skid_signals.c

--- code/src/skid_signals.c
+++ code/src/skid_signals.c
@@ -32,6 +32,20 @@ int call_sigaction(int signum, struct sigaction *newact, struct sigaction *oldac
 
 /*
  *	Description:
+ *		Call sigprocmas() and handle errors in a standardized way.  Input is not validated.
+ *
+ *	Args:
+ *		how: Controls the behavior of sigprocmask() (see: signprocmask(2)).
+ *		set: Pointer to a new signal set.
+ *		oldset: [Optional] Pointer to a storage location for the previous set.
+ *
+ *	Returns:
+ *		On success, ENOERR is returned.  On error, errno is returned.
+ */
+int call_sigprocmask(int how, sigset_t *set, sigset_t *oldset);
+
+/*
+ *	Description:
  *		Initialize action's memory and empty the signal set.
  *
  *	Args:
@@ -42,10 +56,47 @@ int call_sigaction(int signum, struct sigaction *newact, struct sigaction *oldac
  */
 int initialize_sigaction_struct(struct sigaction *action);
 
+/*
+ *	Description:
+ *		Empty this signal set and add signum to it.
+ *
+ *	Notes:
+ *		Use sigemptyset() (see: sigsetops(3)) to initialize a signal set without adding a signal.
+ *
+ *	Args:
+ *		set: The signal set to initialize.
+ *		signum: The signal number to add.
+ *
+ *	Returns:
+ *		On success, ENOERR is returned.  On error, errno is returned.
+ */
+int initialize_signal_set(sigset_t *set, int signum);
+
 
 /**************************************************************************************************/
 /********************************** PUBLIC FUNCTION DEFINITIONS ***********************************/
 /**************************************************************************************************/
+
+
+int block_signal(int signum, sigset_t *oldset)
+{
+	// LOCAL VARIABLES
+	int result = ENOERR;  // Errno value
+	int how = SIG_BLOCK;  // Tells sigprocmask() to block signals
+	sigset_t set;    	  // New signal set (intialized later)
+
+	// BLOCK IT
+	// Initialize the set
+	result = initialize_signal_set(&set, signum);
+	// Call sigprocmask
+	if (ENOERR == result)
+	{
+		result = call_sigprocmask(how, &set, oldset);
+	}
+
+	// DONE
+	return result;
+}
 
 
 int set_signal_handler(int signum, SignalHandler handler, int flags, struct sigaction *oldact)
@@ -84,7 +135,8 @@ int set_signal_handler(int signum, SignalHandler handler, int flags, struct siga
 }
 
 
-int set_signal_handler_ext(int signum, SignalHandlerExt handler, int flags, struct sigaction *oldact)
+int set_signal_handler_ext(int signum, SignalHandlerExt handler, int flags,
+	                       struct sigaction *oldact)
 {
 	// LOCAL VARIABLES
 	int result = ENOERR;      // Errno value
@@ -106,13 +158,34 @@ int set_signal_handler_ext(int signum, SignalHandlerExt handler, int flags, stru
 	if (ENOERR == result)
 	{
 		newact.sa_sigaction = handler;
-		newact.sa_flags = flags;
+		newact.sa_flags = flags | SA_SIGINFO;  // Explicitly adds the flag to use sa_sigaction
 	}
 
 	// SET IT
 	if (ENOERR == result)
 	{
 		result = call_sigaction(signum, &newact, oldact);
+	}
+
+	// DONE
+	return result;
+}
+
+
+int unblock_signal(int signum, sigset_t *oldset)
+{
+	// LOCAL VARIABLES
+	int result = ENOERR;    // Errno value
+	int how = SIG_UNBLOCK;  // Tells sigprocmask() to unblock signals
+	sigset_t set;    	    // New signal set (intialized later)
+
+	// BLOCK IT
+	// Initialize the set
+	result = initialize_signal_set(&set, signum);
+	// Call sigprocmask
+	if (ENOERR == result)
+	{
+		result = call_sigprocmask(how, &set, oldset);
 	}
 
 	// DONE
@@ -129,7 +202,7 @@ int call_sigaction(int signum, struct sigaction *newact, struct sigaction *oldac
 	// LOCAL VARIABLES
 	int result = ENOERR;  // Errno value
 
-	// SET IT
+	// CALL IT
 	if (sigaction(signum, newact, oldact))
 	{
 		result = errno;
@@ -140,6 +213,70 @@ int call_sigaction(int signum, struct sigaction *newact, struct sigaction *oldac
 	// DONE
 	return result;
 }
+
+
+int call_sigaddset(sigset_t *set, int signum)
+{
+	// LOCAL VARIABLES
+	int result = ENOERR;  // Errno value
+
+	// INPUT VALIDATION
+	if (NULL == set)
+	{
+		result = EINVAL;  // NULL pointer
+	}
+
+	// CALL IT
+	if (ENOERR == result)
+	{
+		if(sigaddset(set, signum))
+		{
+			result = errno;
+			PRINT_ERROR(The call to sigaddset() failed);
+			PRINT_ERRNO(result);
+		}
+	}
+
+	// DONE
+	return result;
+}
+
+
+int call_sigemptyset(sigset_t *set)
+{
+	// LOCAL VARIABLES
+	int result = ENOERR;  // Errno value
+
+	// CALL IT
+	if (sigemptyset(set))
+	{
+		result = errno;
+		PRINT_ERROR(The call to sigaction() failed);
+		PRINT_ERRNO(result);
+	}
+
+	// DONE
+	return result;
+}
+
+
+int call_sigprocmask(int how, sigset_t *set, sigset_t *oldset)
+{
+	// LOCAL VARIABLES
+	int result = ENOERR;  // Errno value
+
+	// CALL IT
+	if (sigprocmask(how, set, oldset))
+	{
+		result = errno;
+		PRINT_ERROR(The call to sigprocmask() failed);
+		PRINT_ERRNO(result);
+	}
+
+	// DONE
+	return result;
+}
+
 
 int initialize_sigaction_struct(struct sigaction *action)
 {
@@ -164,6 +301,33 @@ int initialize_sigaction_struct(struct sigaction *action)
 			PRINT_ERROR(The call to sigemptyset() failed);
 			PRINT_ERRNO(result);
 		}
+	}
+
+	// DONE
+	return result;
+}
+
+int initialize_signal_set(sigset_t *set, int signum)
+{
+	// LOCAL VARIABLES
+	int result = ENOERR;  // Errno value
+
+	// INPUT VALIDATION
+	if (NULL == set)
+	{
+		result = EINVAL;  // NULL pointer
+	}
+
+	// INITIALIZE IT
+	// Empty it
+	if (ENOERR == result)
+	{
+		result = call_sigemptyset(set);
+	}
+	// Add signum to the set
+	if (ENOERR == result)
+	{
+		result = call_sigaddset(set, signum);
 	}
 
 	// DONE
