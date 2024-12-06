9b9882a2772057bc89f39c0d8fba826cfdf76a26
hark130 <hark130@yahoo.com>
2024-12-06 13:48:49 -0600

    1-3: Updated SKID's signals library header
    - Updated spaces to tabs
    - Removed handle_all_children() (to be moved to signal_handlers library)
    - Added declaration for set_signal_handler_ext() (a new addition)
    
    Changes to be committed:
    	modified:   code/include/skid_signals.h
    
    Changes not staged for commit:
    	modified:   code/src/skid_signals.c
    
    Untracked files:
    	code/include/skid_signal_handlers.h
    	code/src/skid_signal_handlers.c

--- code/include/skid_signals.h
+++ code/include/skid_signals.h
@@ -1,21 +1,8 @@
 #ifndef __SKID_SIGNALS__
 #define __SKID_SIGNALS__
 
-#include <signal.h>     // Signal MACROs
-
-// Function pointer to be used with struct sigaction's sa_handler member
-typedef void (*SignalHandler)(int signum);
-
-/*
- *	Description:
- *		This signal handler waits for all child processes to exit without hanging by calling:
- *			waitpid(-1, NULL, WNOHANG)
- *		This function conforms with SignalHandler type for use with struct sigaction.sa_handler.
- *
- *	Args:
- *		signum: The signal number.
- */
-void handle_all_children(int signum);
+#include "skid_signal_handlers.h"		// SignalHandler, SignalHandlerExt
+#include <signal.h>     				// Signal MACROs
 
 /*
  *	Description:
@@ -29,5 +16,18 @@ void handle_all_children(int signum);
  *		oldact: [Optional] If non-NULL, the previous action is saved here.
  */
 int set_signal_handler(int signum, SignalHandler handler, int flags, struct sigaction *oldact);
+
+/*
+ *	Description:
+ *		Establish handler as the signum signal handler.
+ *
+ *	Args:
+ *		signum: The signal number.
+ *		handler: Function pointer of the extended signal handler.  Passed to sigaction() by way of
+ *			struct sigaction.sa_sigaction.
+ *		flags: Passed to sigaction() by way of struct sigaction.sa_flags.
+ *		oldact: [Optional] If non-NULL, the previous action is saved here.
+ */
+int set_signal_handler_ext(int signum, SignalHandlerExt handler, int flags, struct sigaction *oldact);
 
 #endif  /* __SKID_SIGNALS__ */
