777ab0029cc9b328423f101a4ecc603ad4ae0925
hark130 <hark130@yahoo.com>
2024-12-13 16:06:44 -0600

    1-3: Defined new extended signal handler
    The queue int read extended signal handler is working as expected.
    
    Changes to be committed:
    	modified:   code/src/skid_signal_handlers.c
    
    Untracked files:
    	code/test/test_ssh_handle_ext_read_queue_int.c

--- code/src/skid_signal_handlers.c
+++ code/src/skid_signal_handlers.c
@@ -17,8 +17,11 @@
 /**************************************************************************************************/
 
 volatile sig_atomic_t skid_sig_hand_interrupted = 0;
+volatile sig_atomic_t skid_sig_hand_data_int = 0;
+volatile sig_atomic_t skid_sig_hand_data_ptr = 0;
 volatile sig_atomic_t skid_sig_hand_ext = 0;
 volatile sig_atomic_t skid_sig_hand_pid = 0;
+volatile sig_atomic_t skid_sig_hand_queue = 0;
 volatile sig_atomic_t skid_sig_hand_sigcode = 0;
 volatile sig_atomic_t skid_sig_hand_signum = 0;
 volatile sig_atomic_t skid_sig_hand_uid = 0;
@@ -26,6 +29,20 @@ volatile sig_atomic_t skid_sig_hand_uid = 0;
 /**************************************************************************************************/
 /********************************* PRIVATE FUNCTION DECLARATIONS **********************************/
 /**************************************************************************************************/
+
+/*
+ *	Description:
+ *		Block a signal.  The result will be the union of the current set and signum.
+ *		It is not possible to block SIGKILL or SIGSTOP.  Attempts to do so are silently ignored.
+ *		This function is reentrant and async-signal-safe.
+ *
+ *	Args:
+ *		signum: The signal number.
+ *
+ *	Returns:
+ *		On success, ENOERR is returned.  On error, errno is returned.
+ */
+int block_signal_safe(int signum);
 
 /**************************************************************************************************/
 /********************************** PUBLIC FUNCTION DEFINITIONS ***********************************/
@@ -82,6 +99,34 @@ void handle_signal_number(int signum)
 /*************************** SA_SIGACTION (SignalHandlerExt) FUNCTIONS ****************************/
 
 
+void handle_ext_read_queue_int(int signum, siginfo_t *info, void *context)
+{
+	// LOCAL VARIABLES
+	union sigval data;              // Store the info->si_value here
+	QueueData_t integer = Integer;  // skid_sig_hand_queue value
+
+	// HANDLE IT
+	if (NULL != info)
+	{
+		if (SI_QUEUE == info->si_code)
+		{
+			// Block further signals
+			if (ENOERR == block_signal_safe(signum))
+			{
+				// Process the data
+				data = info->si_value;
+				skid_sig_hand_signum = info->si_signo;
+				skid_sig_hand_sigcode = info->si_code;
+				skid_sig_hand_pid = info->si_pid;
+				skid_sig_hand_uid = info->si_uid;
+				skid_sig_hand_data_int = data.sival_int;
+				skid_sig_hand_queue = integer;
+			}
+		}
+	}
+}
+
+
 void handle_ext_sending_process(int signum, siginfo_t *info, void *context)
 {
 	if (NULL != info)
@@ -112,3 +157,30 @@ void handle_ext_signal_code(int signum, siginfo_t *info, void *context)
 /**************************************************************************************************/
 /********************************** PRIVATE FUNCTION DEFINITIONS **********************************/
 /**************************************************************************************************/
+
+
+int block_signal_safe(int signum)
+{
+	// LOCAL VARIABLES
+	int result = ENOERR;  // Results of the function call
+	int errnum = errno;   // Preserve errno during this function call
+	sigset_t block_mask;  // Pass this to sigprocmask()
+
+	// BLOCK IT
+	if (sigemptyset(&block_mask))
+	{
+		result = errno;
+	}
+	else if (sigaddset(&block_mask, SIGUSR1))
+	{
+		result = errno;
+	}
+	else if (sigprocmask(SIG_BLOCK, &block_mask, NULL))
+	{
+		result = errno;
+	}
+
+	// DONE
+	errno = errnum;  // Restore errno
+	return result;
+}
