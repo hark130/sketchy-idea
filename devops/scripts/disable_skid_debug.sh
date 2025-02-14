67f29a9797db1cd30e1d3a7752c0da70803b7b35
hark130 <hark130@yahoo.com>
2025-02-14 18:36:11 +0000

    SKID-15: Added pre-cleanup script feature
    DEBUG disable script will now remove all of the old backup files prior
    to continuing (to avoid "false backups").
    
    Changes to be committed:
    	modified:   devops/scripts/disable_skid_debug.sh

--- devops/scripts/disable_skid_debug.sh
+++ devops/scripts/disable_skid_debug.sh
@@ -11,6 +11,7 @@
 
 SRC_CODE_DIR="./code/src"  # Source code directory
 SRC_FILE_EXT=".c"          # Source code file extension
+BAK_FILE_EXT=".bak"        # Backup file extension
 UPDATE_COUNT=0             # Number of files updated
 EXIT_CODE=0                # Exit code var
 
@@ -21,13 +22,29 @@ then
     EXIT_CODE=1
 fi
 
-# 2. Update all the production code
+# 2. Cleanup previously backed up files
+if [[ $EXIT_CODE -eq 0 ]]
+then
+    for skid_backup_file in $(ls $SRC_CODE_DIR/*$BAK_FILE_EXT)
+    do
+        echo "Removing old backup file: $skid_backup_file"
+        rm --force $skid_backup_file
+        EXIT_CODE=$?
+        if [[ $EXIT_CODE -ne 0 ]]
+        then
+            echo "The rm command encountered an error deleting $skid_backup_file"
+            break
+        fi
+    done
+fi
+
+# 3. Update all the production code
 if [[ $EXIT_CODE -eq 0 ]]
 then
     for skid_source_file in $(ls $SRC_CODE_DIR/skid_*$SRC_FILE_EXT)
     do
         # Comments out the SKID_DEBUG macro, in place, after backing up the original source in-place
-        sed -i'./*.bak' 's/^#define SKID_DEBUG/\/\/ #define SKID_DEBUG/g' $skid_source_file
+        sed -i'./*$BAK_FILE_EXT' 's/^#define SKID_DEBUG/\/\/ #define SKID_DEBUG/g' $skid_source_file
         EXIT_CODE=$?
         if [[ $EXIT_CODE -ne 0 ]]
         then
@@ -39,7 +56,7 @@ then
     done
 fi
 
-# 3. Check for changes
+# 4. Provide feedback on changes
 if [[ `git status -s | wc -l` -ne 0 && $UPDATE_COUNT -gt 0 ]]
 then
     echo -e "\nThe following files were modified:"
@@ -47,7 +64,9 @@ then
     echo -e "\nTo undo these changes..."
     echo -e "\tgit restore $SRC_CODE_DIR/*$SRC_FILE_EXT"
     echo -e "\nTo commit these changes..."
-    echo -e "\tgit add $SRC_CODE_DIR/*$SRC_FILE_EXT && git commit\n"
+    echo -e "\tgit add $SRC_CODE_DIR/*$SRC_FILE_EXT && git commit"
+    echo -e "\nThe original files have been backed up..."
+    echo -e "\tls $SRC_CODE_DIR/*$BAK_FILE_EXT"
 fi
 
 # DONE
