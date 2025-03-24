d36596d6eb49ff057ad2e63dd3151a7bb2082ce0
hark130 <hark130@yahoo.com>
2025-03-24 12:35:35 -0500

    11-4: Initial commit of my Query LKM source file
    This is just the basic min-framework.
    This code hasn't been tested, much less compiled, yet.
    I'm troubleshooting my Linux dev VM atm...
    
    Changes to be committed:
    	new file:   modules/query/query.c

---
+++ modules/query/query.c
@@ -0,0 +1,35 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+
+#define DEVICE_NAME "Query LKM"  // Use this macro for logging
+#define DEV_FILENAME "query_me"  // /dev/DEV_FILENAME
+
+
+// NOTES:
+//  Static because there can be only one?
+static int query_init(void)
+{
+    HARKLE_KINFO(Query LKM init);
+    return 0;
+}
+
+static void query_exit(void)
+{
+    HARKLE_KINFO(Query LKM exit);
+    return;
+}
+
+// Tell the Linux kernel where to start executing your Linux module
+// sudo insmod query.ko
+module_init(query_init);
+// Verify the module is loaded with...
+// TO DO: DON'T DO NOW
+// Modules typically live in /lib/modules
+// sudo rmmod query.ko
+module_exit(query_exit);
+
+// DESCRIPTIVE INFORMATION ABOUT THIS MODULE
+MODULE_AUTHOR("Joseph Harkleroad");
+MODULE_DESCRIPTION("IOCTL Queries");
+MODULE_LICENSE("GPL");
