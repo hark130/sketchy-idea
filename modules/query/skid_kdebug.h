19495c1d4bb0b7dd2a6c4bbbab2e9666c14db195
hark130 <hark130@yahoo.com>
2025-03-24 12:34:28 -0500

    11-4: Initial commit of a SKID Kernel Debug header
    Replicated from Lat and updated...
    This code has not been tested (I'm troubleshooting my Linux Dev VM)
    
    Changes to be committed:
    	new file:   modules/query/skid_kdebug.h
    
    Untracked files:
    	modules/query/query.c

---
+++ modules/query/skid_kdebug.h
@@ -0,0 +1,31 @@
+/*
+ *    #define SKID_KDEBUG
+ *    ...in order to enable KERNEL DEBUG logging.
+ */
+
+#include <linux/printk.h>                       // printk()
+
+#ifndef __SKID_KDEBUG_H__
+#define __SKID_KDEBUG_H__
+
+#ifdef SKID_KDEBUG
+#define DEBUG_ERROR_STR "<<<ERROR>>>"
+#define DEBUG_INFO_STR "[INFO]"
+#define DEBUG_WARNG_STR "¿¿¿WARNING???"
+#define HARKLE_KERROR(module, funcName, errMsg) do { printk(KERN_ERR "%s: %s %s - %s\n", DEBUG_ERROR_STR, module, #funcName, errMsg); } while (0);
+#define HARKLE_KERRNO(module, funcName, errNum) do { printk(KERN_ERR "%s: %s %s() returned errno: %d!\n", DEBUG_ERROR_STR, module, #funcName, errNum); } while (0);
+#define HARKLE_KWARNG(module, funcName, warnMsg) do { printk(KERN_WARNING "%s: %s %s() - %s!\n", DEBUG_WARNG_STR, module, #funcName, warnMsg); } while (0);
+#define HARKLE_KFINFO(module, funcName, msg) do { printk(KERN_INFO "%s: %s %s - %s\n", DEBUG_INFO_STR, module, #funcName, msg); } while (0);
+#define HARKLE_KINFO(module, msg) do { printk(KERN_INFO "%s: %s %s\n", DEBUG_INFO_STR, module, msg); } while (0);
+#else
+#define DEBUG_ERROR_STR ""
+#define DEBUG_INFO_STR ""
+#define DEBUG_WARNG_STR ""
+#define HARKLE_KERROR(module, funcName, errMsg) ;;;
+#define HARKLE_KERRNO(module, funcName, errNum) ;;;
+#define HARKLE_KWARNG(module, funcName, warnMsg) ;;;
+#define HARKLE_KFINFO(module, funcName, msg) ;;;
+#define HARKLE_KINFO(module, msg) ;;;
+#endif  /* SKID_KDEBUG */
+
+#endif  /* __SKID_KDEBUG_H__ */
