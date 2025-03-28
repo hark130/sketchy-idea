5e640c4757b21d38715fc98283eeed296d4276be
hark130 <hark130@yahoo.com>
2025-03-28 17:06:34 +0000

    11-4: Renamed the old Lat-Dorsi DEBUG macros
    Now they're named for the SKID repo.
    
    Changes to be committed:
    	modified:   modules/query/skid_kdebug.h
    
    Changes not staged for commit:
    	modified:   modules/Makefile
    	deleted:    modules/Makefile_constants
    	modified:   modules/query/query.c
    
    Untracked files:
    	modules/Makefile_module_constants

--- modules/query/skid_kdebug.h
+++ modules/query/skid_kdebug.h
@@ -12,20 +12,20 @@
 #define DEBUG_ERROR_STR "<<<ERROR>>>"
 #define DEBUG_INFO_STR "[INFO]"
 #define DEBUG_WARNG_STR "¿¿¿WARNING???"
-#define HARKLE_KERROR(module, funcName, errMsg) do { printk(KERN_ERR "%s: %s %s - %s\n", DEBUG_ERROR_STR, module, #funcName, errMsg); } while (0);
-#define HARKLE_KERRNO(module, funcName, errNum) do { printk(KERN_ERR "%s: %s %s() returned errno: %d!\n", DEBUG_ERROR_STR, module, #funcName, errNum); } while (0);
-#define HARKLE_KWARNG(module, funcName, warnMsg) do { printk(KERN_WARNING "%s: %s %s() - %s!\n", DEBUG_WARNG_STR, module, #funcName, warnMsg); } while (0);
-#define HARKLE_KFINFO(module, funcName, msg) do { printk(KERN_INFO "%s: %s %s - %s\n", DEBUG_INFO_STR, module, #funcName, msg); } while (0);
-#define HARKLE_KINFO(module, msg) do { printk(KERN_INFO "%s: %s %s\n", DEBUG_INFO_STR, module, msg); } while (0);
+#define SKID_KERROR(module, funcName, errMsg) do { printk(KERN_ERR "%s: %s %s - %s\n", DEBUG_ERROR_STR, module, #funcName, errMsg); } while (0);
+#define SKID_KERRNO(module, funcName, errNum) do { printk(KERN_ERR "%s: %s %s() returned errno: %d!\n", DEBUG_ERROR_STR, module, #funcName, errNum); } while (0);
+#define SKID_KWARNG(module, funcName, warnMsg) do { printk(KERN_WARNING "%s: %s %s() - %s!\n", DEBUG_WARNG_STR, module, #funcName, warnMsg); } while (0);
+#define SKID_KFINFO(module, funcName, msg) do { printk(KERN_INFO "%s: %s %s - %s\n", DEBUG_INFO_STR, module, #funcName, msg); } while (0);
+#define SKID_KINFO(module, msg) do { printk(KERN_INFO "%s: %s %s\n", DEBUG_INFO_STR, module, msg); } while (0);
 #else
 #define DEBUG_ERROR_STR ""
 #define DEBUG_INFO_STR ""
 #define DEBUG_WARNG_STR ""
-#define HARKLE_KERROR(module, funcName, errMsg) ;;;
-#define HARKLE_KERRNO(module, funcName, errNum) ;;;
-#define HARKLE_KWARNG(module, funcName, warnMsg) ;;;
-#define HARKLE_KFINFO(module, funcName, msg) ;;;
-#define HARKLE_KINFO(module, msg) ;;;
+#define SKID_KERROR(module, funcName, errMsg) ;;;
+#define SKID_KERRNO(module, funcName, errNum) ;;;
+#define SKID_KWARNG(module, funcName, warnMsg) ;;;
+#define SKID_KFINFO(module, funcName, msg) ;;;
+#define SKID_KINFO(module, msg) ;;;
 #endif  /* SKID_KDEBUG */
 
 #endif  /* __SKID_KDEBUG_H__ */
