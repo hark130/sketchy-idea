d65704ddff4793a217ce201c1a8e7943f0c6bc77
hark130 <hark130@yahoo.com>
2025-04-08 15:50:10 +0000

    11-4: Big query.ko step forward
    - Added missing includes
    - Commented redundant(?) includes
    - Added SPOT macros
    - Refactored/defined common functionality into local functions
    	- delete_cdev()
    	- destroy_device()
    	- init_mqd()
    - Added remainig globals: class struct pointer, cdev struct pointer
    - Reordered functions alphabetically
    - Finished defining init() and exit() functions
    
    This code compiles locally, loads, and creates a /dev device as
    expected.
    
    TO DO: DON'T DO NOW...
    - Implement the fops functions
    - Implement IOCTL support
    - Fix the nested build system*
    
    *The query LKM currenlty only builds from the modules/query directory
    
    Changes to be committed:
    	modified:   modules/query/query.c
    
    Changes not staged for commit:
    	modified:   modules/Makefile
    	deleted:    modules/Makefile_constants
    
    Untracked files:
    	modules/Makefile_module_constants

--- modules/query/query.c
+++ modules/query/query.c
@@ -1,9 +1,12 @@
 #define SKID_KDEBUG             // Enable DEBUG logging
 
-#include <linux/errno.h>    // errno macros
-#include <linux/fs.h>       // file_operations struct
-#include <linux/init.h>     // __exit, __init
-#include <linux/module.h>   // module_exit(), module_init()
+#include <linux/cdev.h>     // cdev_add(), cdev_alloc(), cdev_del(), cdev_init()
+// #include <linux/device.h>   // device_create(), device_destroy()
+// #include <linux/errno.h>    // errno macros
+#include <linux/fs.h>       // file_operations struct, various macros, *_chrdev_region()
+// #include <linux/init.h>     // __exit, __init
+#include <linux/module.h>   // module_exit(), module_init(), MODULE_* macros
+#include <linux/version.h>  // KERNEL_VERSION, LINUX_VERSION_CODE
 // #include <sys/lock.h>       // ???
 // #include <sys/sema.h>       // sema_destroy(), sema_init()
 #include "skid_kdebug.h"
@@ -13,10 +16,11 @@
 /********************************************* MACROS *********************************************/
 /**************************************************************************************************/
 
-#define ENOERR ((int)0)          // Success value for errno
-#define DEVICE_NAME "Query LKM"  // Use this macro for logging
-#define DEV_FILENAME "query_me"  // /dev/DEV_FILENAME
 #define CDEV_BUFF_SIZE 511       // Character device buffer size
+#define DEV_FILENAME "query_me"  // /dev/DEV_FILENAME
+#define DEVICE_NAME "Query LKM"  // Use this macro for logging
+#define ENOERR ((int)0)          // Success value for errno
+#define NUM_MIN_NUMS 1           // Number of minor numbers to register/unregister
 
 /**************************************************************************************************/
 /******************************************** TYPEDEFS ********************************************/
@@ -36,6 +40,33 @@ typedef struct _myQueryDevice
 /**************************************************************************************************/
 /************************************* FUNCTION DECLARATIONS **************************************/
 /**************************************************************************************************/
+
+/*
+ *  Description:
+ *      Remove the global cdev from the system by calling cdev_del() and clears the pointer.
+ */
+void delete_cdev(void);
+
+/*
+ *  Description:
+ *      Destroys the global dev_class by calling destroy_device() and clears the pointer.
+ */
+void destroy_device(void);
+
+int device_close(struct inode *inode, struct file *filp);
+
+int device_open(struct inode *inode, struct file *filp);
+
+ssize_t device_read(struct file *filp, char *buf_store_data, size_t buf_count, loff_t *cur_offset);
+
+ssize_t device_write(struct file *filp, const char *buf_src_data, size_t buf_count,
+                     loff_t *cur_offset);
+
+/*
+ *  Description:
+ *      Intialize the myQueryDevice global.
+ */
+void init_mqd(void);
 
 /*
  *  Description:
@@ -68,15 +99,6 @@ static void __exit query_exit(void);
  */
 static int __init query_init(void);
 
-int device_close(struct inode *inode, struct file *filp);
-
-int device_open(struct inode *inode, struct file *filp);
-
-ssize_t device_read(struct file *filp, char *buf_store_data, size_t buf_count, loff_t *cur_offset);
-
-ssize_t device_write(struct file *filp, const char *buf_src_data, size_t buf_count,
-                     loff_t *cur_offset);
-
 /**************************************************************************************************/
 /******************************************** GLOBALS *********************************************/
 /**************************************************************************************************/
@@ -91,56 +113,35 @@ struct file_operations fops =
     .read = device_read       // Points to the method to call when reading from the device
 };
 
-myQueryDevice my_query_device;  // My Query Device
+myQueryDevice my_query_device;          // My Query Device
+
+static struct class *dev_class = NULL;  // Character device class
+
+struct cdev *query_cdev = NULL;         // Character device driver struct
 
 /**************************************************************************************************/
 /************************************** FUNCTION DEFINITIONS **************************************/
 /**************************************************************************************************/
 
 
-static void __exit query_exit(void)
+void delete_cdev(void)
 {
-    // EXIT
-    SKID_KINFO(DEVICE_NAME, "LKM exit");
-    // Teardown Character Device
-    // 1. Unregister the character device
-    SKID_KINFO(DEVICE_NAME, "Unregistering the character device");
-
-    // DONE
-    return;
+    if (NULL != query_cdev)
+    {
+        cdev_del(query_cdev);  // Remove the cdev from the system
+        query_cdev = NULL;     // Clear the pointer
+    }
 }
 
 
-static int __init query_init(void)
+void destroy_device(void)
 {
-    // LOCAL VARIABLES
-    int result = ENOERR;  // Function call results
-
-    // INIT
-    SKID_KINFO(DEVICE_NAME, "LKM init");
-    // Setup Character Device
-    // 1. Prepare the internal struct
-    SKID_KINFO(DEVICE_NAME, "Preparing the internal struct");
-    memset(my_query_device.log_buf, 0x0, (CDEV_BUFF_SIZE + 1) * sizeof(char));
-    // Initialize the semaphores
-    sema_init(&my_query_device.open_sem, 1);
-    sema_init(&my_query_device.busy_sem, 1);
-
-    // 2. Register the character device
-    // dynamically chooses the major device number
-    SKID_KINFO(DEVICE_NAME, "Registering the character device");
-
-
-// 65 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 4, 0)
-// 66 cls = class_create(DEVICE_NAME);
-// 67 #else
-// 68 cls = class_create(THIS_MODULE, DEVICE_NAME);
-// 69 #endif
-// 70 device_create(cls, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
- 
-
-    // DONE
-    return result;
+    if (NULL != dev_class)
+    {
+        // Removes a device that was created with device_create
+        device_destroy(dev_class, my_query_device.dev_num);
+        dev_class = NULL;  // Clear the pointer
+    }
 }
 
 
@@ -179,6 +180,146 @@ ssize_t device_write(struct file *filp, const char *buf_src_data, size_t buf_cou
 {
     // LOCAL VARIABLES
     ssize_t result = 0;
+
+    // DONE
+    return result;
+}
+
+
+void init_mqd(void)
+{
+    memset(my_query_device.log_buf, 0x0, (CDEV_BUFF_SIZE + 1) * sizeof(char));
+    my_query_device.buf_len = 0;
+    // Initialize the semaphores
+    sema_init(&(my_query_device.open_sem), 1);
+    sema_init(&(my_query_device.busy_sem), 1);
+    my_query_device.dev_num = 0;
+    my_query_device.major_num = 0;
+    my_query_device.minor_num = 0;
+}
+
+
+static void __exit query_exit(void)
+{
+    // LOCAL VARIABLES
+    unsigned int count = NUM_MIN_NUMS;  // Number of minor numbers to unregister
+
+    // EXIT
+    SKID_KINFO(DEVICE_NAME, "exit");
+    // Teardown Character Device
+    // 1. Destroy the character device
+    SKID_KINFO(DEVICE_NAME, "Destroying the character device");  // init() #6
+    destroy_device();
+    // 2. Remove the cdev
+    SKID_KINFO(DEVICE_NAME, "Removing the cdev from the system");
+    delete_cdev();  // init() #4 & #5
+    // 3. Destroy the device class
+    SKID_KINFO(DEVICE_NAME, "Destroying device class");
+    class_destroy(dev_class);  // init() #3
+    // 4. Unregister the character device
+    SKID_KINFO(DEVICE_NAME, "Unregistering the character device");
+    unregister_chrdev_region(my_query_device.dev_num, count);  // init() #2
+
+    // DONE
+    return;
+}
+
+
+static int __init query_init(void)
+{
+    // LOCAL VARIABLES
+    int result = ENOERR;                // Function call results
+    unsigned int baseminor = 0;         // First of the requested range of minor numbers
+    unsigned int count = NUM_MIN_NUMS;  // Number of minor numbers to register
+    struct device *device = NULL;       // device_create() return value
+
+    // INIT
+    SKID_KINFO(DEVICE_NAME, "init");
+    // Setup Character Device
+    // 1. Prepare the internal struct
+    SKID_KINFO(DEVICE_NAME, "Preparing the internal struct");
+    init_mqd();  // Initialize the myQueryDevice struct
+    // 2. Register the character device
+    SKID_KINFO(DEVICE_NAME, "Registering the character device");
+    // dynamically chooses the major device number
+    result = alloc_chrdev_region(&(my_query_device.dev_num), baseminor, count, DEVICE_NAME);
+    if (ENOERR != result)
+    {
+        SKID_KERROR(DEVICE_NAME, "The call to alloc_chrdev_region() failed");
+        SKID_KERRNO(DEVICE_NAME, -result);
+    }
+    else
+    {
+        my_query_device.major_num = MAJOR(my_query_device.dev_num);
+        my_query_device.minor_num = MINOR(my_query_device.dev_num);
+        printk(KERN_INFO "%s received major %d minor %d\n", DEVICE_NAME, my_query_device.major_num,
+               my_query_device.minor_num);
+    }
+
+    // 3. Create device class (before allocation of the array of devices)
+    if (ENOERR == result)
+    {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 4, 0)
+        dev_class = class_create(DEVICE_NAME);
+#else
+        dev_class = class_create(THIS_MODULE, DEVICE_NAME);
+#endif
+        if (IS_ERR(dev_class))
+        {
+            SKID_KERROR(DEVICE_NAME, "The call to class_create() failed");
+            result = PTR_ERR(dev_class);
+        }
+    }
+
+    // 4. Allocate and initialize the character device structure
+    if (ENOERR == result)
+    {
+        // Create our cdev structure
+        query_cdev = cdev_alloc();
+
+        // Initialize the cdev structure
+        if (NULL == query_cdev)
+        {
+            SKID_KERROR(DEVICE_NAME,
+                        "The call to cdev_alloc() failed to allocate the cdev structure");
+            result = -1;
+        }
+        else
+        {
+            cdev_init(query_cdev, &fops);
+        }
+    }
+
+    // 5. Add this character device to the system
+    if (ENOERR == result)
+    {
+        result = cdev_add(query_cdev, my_query_device.dev_num, count);
+
+        if (ENOERR != result)
+        {
+            SKID_KERROR(DEVICE_NAME, "Unable to add cdev to kernel");
+            SKID_KERRNO(DEVICE_NAME, -result);
+        }
+        else
+        {
+            // The device is now "live" and can be called by the kernel
+            SKID_KINFO(DEVICE_NAME, "Character device is now 'live'");
+        }
+    }
+
+    // 6. Create a device and register it with sysfs
+    if (ENOERR == result)
+    {
+        device = device_create(dev_class, NULL, my_query_device.dev_num, NULL, DEV_FILENAME);
+
+        if (IS_ERR(device))
+        {
+            result = PTR_ERR(device);
+            SKID_KERROR(DEVICE_NAME, "The call to device_create() failed");
+            SKID_KERRNO(DEVICE_NAME, result);
+            delete_cdev();
+        }
+    }
 
     // DONE
     return result;
