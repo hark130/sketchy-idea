0bbf7936351c87abd84151343b708a3c6dc6fefe
hark130 <hark130@yahoo.com>
2025-06-11 17:22:35 +0000

    8-4: Converted tabs to spaces
    How does this keep happening?!
    
    Changes to be committed:
    	modified:   code/test/test_libskid_sdo_read_dir_contents.c
    	modified:   code/test/test_libskid_sfmr_get_file_perms.c

--- code/test/test_libskid_sdo_read_dir_contents.c
+++ code/test/test_libskid_sdo_read_dir_contents.c
@@ -1,8 +1,8 @@
 /*
- *	Manually test skid_dir_operations' read_dir_contents()...
+ *  Manually test skid_dir_operations' read_dir_contents()...
  *  ...using the libsketchyidea shared object.
  *
- *	Copy/paste the following...
+ *  Copy/paste the following...
 
 make
 make install
@@ -11,150 +11,150 @@ make install
  *
  */
 
-#define SKID_DEBUG						// Enable DEBUG logging
+#define SKID_DEBUG                      // Enable DEBUG logging
 
-#include <errno.h>						// EINVAL
-#include <stdlib.h>						// exit()
-#include "skid_debug.h"					// FPRINTF_ERR(), PRINT_ERRNO(), PRINT_ERROR()
-#include "skid_dir_operations.h"		// read_dir_contents()
-#include "skid_file_metadata_read.h"	// is_directory()
-#include "skid_macros.h"				// ENOERR
+#include <errno.h>                      // EINVAL
+#include <stdlib.h>                     // exit()
+#include "skid_debug.h"                 // FPRINTF_ERR(), PRINT_ERRNO(), PRINT_ERROR()
+#include "skid_dir_operations.h"        // read_dir_contents()
+#include "skid_file_metadata_read.h"    // is_directory()
+#include "skid_macros.h"                // ENOERR
 
 /*
- *	Description:
- *		Print num_entries number of entries from dir_contents.
+ *  Description:
+ *      Print num_entries number of entries from dir_contents.
  *
- *	Args:
- *		dir_name: The name of the directory the contents were read from.
- *		dir_contents: An NULL-terminate array of string pointers, on success.
- *			May be NULL (for an empty dir).
- *		num_entries: The total number of indices, 0 or otherwise, in dir_contents.
+ *  Args:
+ *      dir_name: The name of the directory the contents were read from.
+ *      dir_contents: An NULL-terminate array of string pointers, on success.
+ *          May be NULL (for an empty dir).
+ *      num_entries: The total number of indices, 0 or otherwise, in dir_contents.
  *
- *	Returns:
- *		ENOERR on success, errno on failure.  EIO is used to indicate a mismatch between the
- *		NULL-terminated dir_contents and num_entries.
+ *  Returns:
+ *      ENOERR on success, errno on failure.  EIO is used to indicate a mismatch between the
+ *      NULL-terminated dir_contents and num_entries.
  */
 int print_contents(char *dir_name, char **dir_contents, size_t num_entries);
 
 /*
- *	Description:
- *		Print the "help" usage for this manual test code.
+ *  Description:
+ *      Print the "help" usage for this manual test code.
  *
- *	Args:
- *		prog_name: Pass in argv[0] here.
+ *  Args:
+ *      prog_name: Pass in argv[0] here.
  */
 void print_usage(const char *prog_name);
 
 
 int main(int argc, char *argv[])
 {
-	// LOCAL VARIABLES
-	int exit_code = 0;           // Store errno and/or results here
-	bool recurse = false;        // Recurse argument value for read_dir_contents()
-	char **dir_contents = NULL;  // Pointer to an array of character pointers
-	size_t num_entries = 0;      // Number of entries in the dir_contents array
+    // LOCAL VARIABLES
+    int exit_code = 0;           // Store errno and/or results here
+    bool recurse = false;        // Recurse argument value for read_dir_contents()
+    char **dir_contents = NULL;  // Pointer to an array of character pointers
+    size_t num_entries = 0;      // Number of entries in the dir_contents array
 
-	// INPUT VALIDATION
-	if (argc != 2)
-	{
-		print_usage(argv[0]);
-		exit_code = EINVAL;
-	}
-	else if (false == is_directory(argv[1], &exit_code))
-	{
-		fprintf(stderr, "'%s' is not a directory.\n%s\n", argv[1], strerror(exit_code));
-		print_usage(argv[0]);
-		if (ENOERR == exit_code)
-		{
-			exit_code = ENOTDIR;  // Just in case
-		}
-	}
-	
-	// READ IT
-	if (ENOERR == exit_code)
-	{
-		dir_contents = read_dir_contents(argv[1], recurse, &exit_code, &num_entries);
-		if (ENOERR == exit_code)
-		{
-			exit_code = print_contents(argv[1], dir_contents, num_entries);
-		}
-		else
-		{
-			PRINT_ERROR(The call to read_dir_contents() failed);
-			PRINT_ERRNO(exit_code);
-		}
-	}
+    // INPUT VALIDATION
+    if (argc != 2)
+    {
+        print_usage(argv[0]);
+        exit_code = EINVAL;
+    }
+    else if (false == is_directory(argv[1], &exit_code))
+    {
+        fprintf(stderr, "'%s' is not a directory.\n%s\n", argv[1], strerror(exit_code));
+        print_usage(argv[0]);
+        if (ENOERR == exit_code)
+        {
+            exit_code = ENOTDIR;  // Just in case
+        }
+    }
 
-	// CLEANUP
-	free_skid_dir_contents(&dir_contents);  // Best effort
+    // READ IT
+    if (ENOERR == exit_code)
+    {
+        dir_contents = read_dir_contents(argv[1], recurse, &exit_code, &num_entries);
+        if (ENOERR == exit_code)
+        {
+            exit_code = print_contents(argv[1], dir_contents, num_entries);
+        }
+        else
+        {
+            PRINT_ERROR(The call to read_dir_contents() failed);
+            PRINT_ERRNO(exit_code);
+        }
+    }
 
-	// DONE
-	exit(exit_code);
+    // CLEANUP
+    free_skid_dir_contents(&dir_contents);  // Best effort
+
+    // DONE
+    exit(exit_code);
 }
 
 
 int print_contents(char *dir_name, char **dir_contents, size_t num_entries)
 {
-	// LOCAL VARIABLES
-	int errnum = ENOERR;    // Errno value to return
-	char **tmp_ptr = NULL;  // Iterating variable
-	int count = 0;          // Number of entries found in dir_contents
+    // LOCAL VARIABLES
+    int errnum = ENOERR;    // Errno value to return
+    char **tmp_ptr = NULL;  // Iterating variable
+    int count = 0;          // Number of entries found in dir_contents
 
-	// INPUT VALIDATION
-	if (NULL == dir_name || 0x0 == *dir_name)
-	{
-		errnum = EINVAL;  // Bad string
-	}
-	if (NULL == dir_contents && 0 != num_entries)
-	{
-		FPRINTF_ERR("Directory contents are empty but num_entries is %lu\n", num_entries);
-		errnum = EIO;  // Mismatch
-	}
-	else if (NULL != dir_contents && 0 == num_entries)
-	{
-		FPRINTF_ERR("Directory contents exist but num_entries is %lu\n", num_entries);
-		errnum = EIO;  // Mismatch
-	}
-	else
-	{
-		// Validate length
-		tmp_ptr = dir_contents;
-		while (tmp_ptr && *tmp_ptr)
-		{
-			count++;  // Increment the count
-			tmp_ptr++;  // Next position
-		}
-		if (count >= num_entries && count > 0)
-		{
-			FPRINTF_ERR("Directory content count is %d but num_entries is %lu\n",
-				        count, num_entries);
-			errnum = EIO;  // Mismatch
-		}
-	}
+    // INPUT VALIDATION
+    if (NULL == dir_name || 0x0 == *dir_name)
+    {
+        errnum = EINVAL;  // Bad string
+    }
+    if (NULL == dir_contents && 0 != num_entries)
+    {
+        FPRINTF_ERR("Directory contents are empty but num_entries is %lu\n", num_entries);
+        errnum = EIO;  // Mismatch
+    }
+    else if (NULL != dir_contents && 0 == num_entries)
+    {
+        FPRINTF_ERR("Directory contents exist but num_entries is %lu\n", num_entries);
+        errnum = EIO;  // Mismatch
+    }
+    else
+    {
+        // Validate length
+        tmp_ptr = dir_contents;
+        while (tmp_ptr && *tmp_ptr)
+        {
+            count++;  // Increment the count
+            tmp_ptr++;  // Next position
+        }
+        if (count >= num_entries && count > 0)
+        {
+            FPRINTF_ERR("Directory content count is %d but num_entries is %lu\n",
+                        count, num_entries);
+            errnum = EIO;  // Mismatch
+        }
+    }
 
-	// PRINT CONTENTS
-	if (ENOERR == errnum)
-	{
-		printf("The contents of '%s':\n", dir_name);
-		if (NULL == dir_contents)
-		{
-			printf("\t[EMPTY]\n");
-		}
-		else
-		{
-			for (int i = 0; i < num_entries && dir_contents[i]; i++)
-			{
-				printf("\t%s\n", dir_contents[i]);
-			}
-		}
-	}
+    // PRINT CONTENTS
+    if (ENOERR == errnum)
+    {
+        printf("The contents of '%s':\n", dir_name);
+        if (NULL == dir_contents)
+        {
+            printf("\t[EMPTY]\n");
+        }
+        else
+        {
+            for (int i = 0; i < num_entries && dir_contents[i]; i++)
+            {
+                printf("\t%s\n", dir_contents[i]);
+            }
+        }
+    }
 
-	// DONE
-	return errnum;
+    // DONE
+    return errnum;
 }
 
 
 void print_usage(const char *prog_name)
 {
-	fprintf(stderr, "Usage: %s <DIRECTORY>\n", prog_name);
+    fprintf(stderr, "Usage: %s <DIRECTORY>\n", prog_name);
 }
--- code/test/test_libskid_sfmr_get_file_perms.c
+++ code/test/test_libskid_sfmr_get_file_perms.c
@@ -1,8 +1,8 @@
 /*
- *	Manually test skid_file_metadata_read.h's get_file_perms() function...
+ *  Manually test skid_file_metadata_read.h's get_file_perms() function...
  *  ...using the libsketchyidea shared object.
  *
- *	Copy/paste the following...
+ *  Copy/paste the following...
 
 make
 make install
@@ -10,15 +10,15 @@ make install
 ./code/dist/test_sfmr_get_file_perms.bin ./code/test/test_input/
 ./code/dist/test_sfmr_get_file_perms.bin ./code/test/test_input/sym_link.txt
 
- *	NOTE: A symbolic link identifies as a regular file here because stat() follows symbolic links.
- *		Use lstat() to positively identify symbolic links.
+ *  NOTE: A symbolic link identifies as a regular file here because stat() follows symbolic links.
+ *      Use lstat() to positively identify symbolic links.
  */
 
 // Standard includes
 #include <errno.h>                    // EINVAL
-#include <stdint.h>					  // uintmax_t
+#include <stdint.h>                   // uintmax_t
 #include <stdio.h>                    // fprintf()
-#include <stdlib.h>					  // exit()
+#include <stdlib.h>                   // exit()
 // Local includes
 #define SKID_DEBUG                    // The DEBUG output is doing double duty as test output
 #include "skid_debug.h"               // PRINT_ERRNO(), PRINT_ERROR()
@@ -27,37 +27,37 @@ make install
 
 int main(int argc, char *argv[])
 {
-	// LOCAL VARIABLES
-	int exit_code = 0;      // Store errno and/or results here
-	mode_t answer = 0;      // Return value from get_file_perms()
-	char *pathname = NULL;  // Get this from argv[1]
+    // LOCAL VARIABLES
+    int exit_code = 0;      // Store errno and/or results here
+    mode_t answer = 0;      // Return value from get_file_perms()
+    char *pathname = NULL;  // Get this from argv[1]
 
-	// INPUT VALIDATION
-	if (argc != 2)
-	{
-	   fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
-	   exit_code = EINVAL;
-	}
-	else
-	{
-		pathname = argv[1];
-	}
+    // INPUT VALIDATION
+    if (argc != 2)
+    {
+       fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
+       exit_code = EINVAL;
+    }
+    else
+    {
+        pathname = argv[1];
+    }
 
-	// CHECK IT
-	if (!exit_code)
-	{
-		answer = get_file_perms(pathname, &exit_code);
-		if (exit_code)
-		{
-			PRINT_ERROR(The call to get_file_perms() failed);
-			PRINT_ERRNO(exit_code);
-		}
-		else
-		{
-			printf("%s has the following permissions: %jo (octal).\n", pathname, (uintmax_t)answer);
-		}
-	}
+    // CHECK IT
+    if (!exit_code)
+    {
+        answer = get_file_perms(pathname, &exit_code);
+        if (exit_code)
+        {
+            PRINT_ERROR(The call to get_file_perms() failed);
+            PRINT_ERRNO(exit_code);
+        }
+        else
+        {
+            printf("%s has the following permissions: %jo (octal).\n", pathname, (uintmax_t)answer);
+        }
+    }
 
-	// DONE
-	exit(exit_code);
+    // DONE
+    exit(exit_code);
 }
