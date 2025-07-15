200d22c61f8b7a1f9ded60e5b48616b68ca239f6
hark130 <hark130@yahoo.com>
2025-07-15 19:55:57 +0000

    8-5: Initial commit of nostdlib code
    I had to change the name from test_misc_gcc_options because the build
    system automatically picks up on source files that start with test_ and
    tries to link them.
    
    This source file needs a bespoke Makefile rule to get it where it needs
    to go.
    
    The manual command in the file comment block is correct, and works, but
    it's missing a few things.
    
    - The comment is stale (it's not testing skid_assembly anymore)
    - It needs the code to acutally execute the file
    - It needs a bit more of an explanation
    
    This branch also needs
    - An export script
    
    Changes to be committed:
    	new file:   code/test/showcase_gcc_options.c

---
+++ code/test/showcase_gcc_options.c
@@ -0,0 +1,49 @@
+/*
+ *  Manually test skid_assembly's inline assembly functionality.
+ *  This binary makes use of call_write() to write to stdout.
+ *
+ *  Copy/paste the following...
+
+gcc -nostdlib -o ./code/dist/showcase_gcc_options.bin ./code/test/showcase_gcc_options.c -static
+
+ *
+ */
+
+#include <stddef.h>                     // size_t
+#include <unistd.h>                     // ssize_t
+
+#define MESSAGE "Hello from test_sa_call_write\n"
+
+
+ssize_t call_write(int fildes, const void *buf, size_t nbyte)
+{
+    // LOCAL VARIABLES
+    ssize_t num_bytes = -1;  // Number of bytes written by the call to write
+
+#if defined(__x86_64__) // Intel x86 Family
+    // WRITE IT
+    __asm__ __volatile__ ("movq $1, %%rax\nsyscall"
+                          : "=a" (num_bytes)
+                          : "D" (fildes), "S" (buf), "d" (nbyte)
+                          : "rcx", "r11", "memory");
+#endif  /* __x86_64__ */
+
+    // DONE
+    return num_bytes;
+}
+
+
+void _start()
+{
+    char buf[] = { MESSAGE };  // String to write
+    call_write(1, buf, sizeof(buf) - 1);
+
+#if defined(__x86_64__)  // Intel x86-64
+    __asm__ volatile ("mov $60, %%rax\n"
+                      "xor %%rdi, %%rdi\n"
+                      "syscall\n"
+                      ::: "%rax", "%rdi");
+#else
+#error "This function does not support the current architecture."
+#endif  /* Built-in Architecture Macros */
+}
