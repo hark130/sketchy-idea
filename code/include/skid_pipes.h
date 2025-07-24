d13cfc4ca18b0cc911b4c7a852fa131958dd3a08
hark130 <hark130@yahoo.com>
2025-07-23 19:35:13 -0500

    4-3: Initial(ish) commit of skid_pipes
    Updated the header:
    - Added new function prototypes
    	- close_pipe()
    	- delete_named_pipe()
    - Changed make_named_pipe() declaration
    
    Defined the functions.
    
    Hasn't been compiled yet, much less tested.
    
    Changes to be committed:
    	modified:   code/include/skid_pipes.h
    	new file:   code/src/skid_pipes.c
    
    Changes not staged for commit:
    	modified:   code/test/test_sn_simple_stream_server.c
    	modified:   code/test/test_sn_socket_logging_server.c
    	modified:   code/test/test_sp_named_pipe_server.c

--- code/include/skid_pipes.h
+++ code/include/skid_pipes.h
@@ -1,18 +1,49 @@
 #ifndef __SKID_PIPES__
 #define __SKID_PIPES__
 
+#include <stdbool.h>                        // bool
+
 /*
  *  Description:
- *      Make a FIFO special file with name pathname and permissions mode.
+ *      Close a pipe file descriptor and sets it to SKID_BAD_FD (if it was successfully closed).
  *
  *  Args:
- *		pathname: Absolute or relative pathname for the new FIFO special file.
- *		mode: Specifies pathname's permissions.  See: skid_macros.h for mode macros.
+ *      pipefd: [In/Out] A pointer to a pipe file descriptor to close.
+ *      quiet: If true, silences all logging/debugging.
+ *
+ *  Returns:
+ *      On success, zero is returned.  On error, errno is returned.
+ */
+int close_pipe(int *pipefd, bool quiet);
+
+/*
+ *  Description:
+ *      Delete a named pipe by calling unlink().
+ *
+ *  Notes:
+ *      The name for it is removed but processes which have the object open may continue to use it.
+ *
+ *  Args:
+ *      filename: Absolute or relative named socket to delete.
+ *
+ *  Returns:
+ *      On success, zero is returned.  On error, errno is returned.
+ */
+int delete_named_pipe(const char *pathname);
+
+/*
+ *  Description:
+ *      Make a FIFO special file with name pathname and permissions mode and open it.
+ *
+ *  Args:
+ *      pathname: Absolute or relative pathname for the new FIFO special file.
+ *      mode: Specifies pathname's permissions.  See: skid_macros.h for mode macros.
  *      errnum: [Out] Storage location for errno values encountered.
  *
  *  Returns:
- *      ENOERR, on success.  On failure, an errno value.  Uses EEXIST if pathname exists.
+ *      ENOERR, on success.  On failure, an errno value.  Uses EEXIST if pathname exists and
+ *      EPIPE for an unspecified error.
  */
-int make_named_pipe(const char *pathname, mode_t mode, int *errnum);
+int make_named_pipe(const char *pathname, mode_t mode);
 
 #endif  /* __SKID_PIPES__ */
---
+++ code/src/skid_pipes.c
@@ -0,0 +1,106 @@
+/*
+ *  This library defines functionality to help manage pipes.
+ */
+
+
+#include <errno.h>                          // errno, EEXIST
+#include <stdbool.h>                        // false
+#include "skid_debug.h"                     // PRINT_E*(), MODULE_*LOAD()
+#include "skid_file_descriptors.h"          // close_fd()
+#include "skid_file_metadata_read.h"        // is_path()
+#include "skid_validation.h"                // validate_skid_err(), validate_skid_pathname()
+
+MODULE_LOAD();  // Print the module name being loaded using the gcc constructor attribute
+MODULE_UNLOAD();  // Print the module name being unloaded using the gcc destructor attribute
+
+
+/**************************************************************************************************/
+/********************************* PRIVATE FUNCTION DECLARATIONS **********************************/
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/********************************** PUBLIC FUNCTION DEFINITIONS ***********************************/
+/**************************************************************************************************/
+
+
+int close_pipe(int *pipefd, bool quiet)
+{
+    // LOCAL VARIABLES
+    int result = ENOERR;  // Errno values
+
+    // INPUT VALIDATION
+    // Handled by close_fd()
+
+    // CLOSE IT
+    result = close_fd(pipefd, quiet);
+
+    // DONE
+    return result;
+}
+
+
+int delete_named_pipe(const char *pathname)
+{
+    // LOCAL VARIABLES
+    int result = ENOERR;  // Errno values
+
+    // INPUT VALIDATION
+    // Handled by delete_file()
+
+    // CLOSE IT
+    result = delete_file(pathname);
+
+    // DONE
+    return result;
+}
+
+
+int make_named_pipe(const char *pathname, mode_t mode)
+{
+    // LOCAL VARIABLES
+    int result = ENOERR;  // Results of execution
+
+    // INPUT VALIDATION
+    result = validate_skid_pathname(pathname, false);
+    if (ENOERR == result)
+    {
+        result = validate_skid_err(errnum);
+    }
+
+    // ENVIRONMENT VALIDATION
+    if (ENOERR == result)
+    {
+        if (true == is_path(pathname, &result))
+        {
+            result = EEXIST;  // We will not overwrite it
+        }
+        else
+        {
+            result = ENOERR;  // Ignore other errors
+        }
+    }
+
+    // MAKE IT
+    if (ENOERR == result)
+    {
+        if (mkfifo(pathname, mode))
+        {
+            result = errno;
+            PRINT_ERROR(The call to mkfifo() failed);
+            if (ENOERR == result)
+            {
+                PRINT_ERROR(Unspecified error replaced with broken pipe errno);
+                result = EPIPE;  // Unspecified error
+            }
+            PRINT_ERRNO(result);
+        }
+    }
+
+    // DONE
+    return result;
+}
+
+
+/**************************************************************************************************/
+/********************************** PRIVATE FUNCTION DEFINITIONS **********************************/
+/**************************************************************************************************/
